61b549bdf978e2b3a78f21b3634e86af409fd30c
Dale Emery <dale@dhemery.com>
2018-01-26 22:04:47 -0800

    Minor name fixes

--- src/util/sigmoid.h
+++ src/util/sigmoid.h
@@ -9,12 +9,12 @@
 namespace DHE {
 
 inline float sigmoid(float x, float curvature) {
-  static constexpr float precision = 1e-4f;
-  static constexpr float maxCurvature = 1.0f - precision;
+  static constexpr float precision{1e-4f};
+  static constexpr float maxCurvature{1.0f - precision};
   static const Range curvatureRange{-maxCurvature, maxCurvature};
-  float k = curvatureRange.clamp(curvature);
+  curvature = curvatureRange.clamp(curvature);
   x = BIPOLAR_NORMAL.clamp(x);
-  return (x - x*k)/(k - abs(x)*2.0f*k + 1.0f);
+  return (x - x*curvature)/(curvature - abs(x)*2.0f*curvature + 1.0f);
 }
 }
 #endif
