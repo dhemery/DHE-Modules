60362370594ba4f5507187bfdee298d1ecf65d87
Dale Emery <dale@dhemery.com>
2018-01-31 18:01:27 -0800

    More auto, const, and other cleanup

--- src/gui/widget.cpp
+++ src/gui/widget.cpp
@@ -23,29 +23,31 @@ Widget::Widget(rack::Module *module, int widget_hp, const char *background) {
 }
 
 void Widget::install_screws() {
-  auto left_x{std::min(width()/4, MAX_SCREW_INSET)}; // One HP from edge
-  auto top_y{7.5f}; // Screw touches top of module
+  static constexpr float max_screw_inset{RACK_GRID_WIDTH*1.5f};
+  static constexpr float top_y{7.5f}; // Screw touches top and bottom of module
 
-  auto right_x = width() - left_x;
-  auto bottom_y = height() - top_y;
-  std::vector<rack::Vec> screw_positions{
-      rack::Vec(left_x, top_y),
-      rack::Vec(left_x, bottom_y),
-      rack::Vec(right_x, top_y),
-      rack::Vec(right_x, bottom_y)
+  auto left_x{std::min(width()/4, max_screw_inset)}; // One HP from edge
+  auto right_x{width() - left_x};
+  auto bottom_y{height() - top_y};
+
+  auto screw_positions = std::vector<rack::Vec>{
+      {left_x, top_y},
+      {left_x, bottom_y},
+      {right_x, top_y},
+      {right_x, bottom_y}
   };
 
-  auto screw_count = screw_positions.size();
   std::random_device rd;
+  auto screw_count = screw_positions.size();
   auto zero_through_three = std::uniform_int_distribution<unsigned long>{0, 3};
 
   auto black_screw_index = zero_through_three(rd);
 
-  for (auto i = 0UL; i < screw_count; i++) {
-    if (i==black_screw_index) {
-      install_screw<rack::ScrewBlack>(screw_positions[i]);
+  for (auto screw_index = 0UL; screw_index < screw_count; screw_index++) {
+    if (screw_index==black_screw_index) {
+      install_screw<rack::ScrewBlack>(screw_positions[screw_index]);
     } else {
-      install_screw<rack::ScrewSilver>(screw_positions[i]);
+      install_screw<rack::ScrewSilver>(screw_positions[screw_index]);
     }
   }
 }
--- src/gui/widget.h
+++ src/gui/widget.h
@@ -7,8 +7,6 @@
 #include "rack.hpp"
 
 namespace DHE {
-static constexpr float MAX_SCREW_INSET{RACK_GRID_WIDTH*1.5f};
-
 class Widget : public rack::ModuleWidget {
 
 public:
@@ -16,14 +14,14 @@ public:
   Widget(rack::Module *module, int widget_hp, const char *background);
 
   rack::Vec center() {
-    return box.getCenter();
+  return box.getCenter();
   }
 
-  float height() {
+  float height() const {
     return box.size.y;
   }
 
-  float width() {
+  float width() const {
     return box.size.x;
   }
 
@@ -36,21 +34,21 @@ public:
 
   template<class T>
   void install_output(int index, float x, float y) {
-    auto output{rack::createOutput<T>(rack::Vec(0, 0), module, index)};
+    auto output{rack::createOutput<T>({0, 0}, module, index)};
     moveTo(output->box, x, y);
     addOutput(output);
   }
 
   template<class T>
   void install_param(int index, float x, float y, float initial = 0.5f) {
-    auto param{rack::createParam<T>(rack::Vec(x, y), module, index, 0.0f, 1.0f, initial)};
+    auto param{rack::createParam<T>({x, y}, module, index, 0.0f, 1.0f, initial)};
     moveTo(param->box, x, y);
     addParam(param);
   }
 
   template<class T>
   void install_light(int index, float x, float y, float initial = 0.5f) {
-    auto light{rack::createLight<T>(rack::Vec(x, y), module, index)};
+    auto light{rack::createLight<T>({x, y}, module, index)};
     moveTo(light->box, x, y);
     addChild(light);
   }
@@ -62,20 +60,20 @@ public:
 
   template<class T>
   void install_screw(rack::Vec pos) {
-    auto widget{rack::createScrew<T>(rack::Vec(0, 0))};
+    auto widget{rack::createScrew<T>({0, 0})};
     moveTo(widget->box, pos);
     addChild(widget);
   }
 
-  void install_screws();
-
   static void moveTo(rack::Rect &box, rack::Vec pos) {
-    box.pos = pos.minus(box.size.div(2));
+    box.pos = pos.minus(box.size.mult(0.5f));
   }
 
   static void moveTo(rack::Rect &box, float x, float y) {
-    moveTo(box, rack::Vec(x, y));
+    moveTo(box, {x, y});
   }
+
+  void install_screws();
 };
 
 }
