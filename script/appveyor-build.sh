3b74e0873da7060491b735a5802711c856b05b7d
Dale Emery <dale@dhemery.com>
2018-02-14 13:57:15 -0800

    On AV: RACK_DIR=/tmp/Rack

--- script/appveyor-build.sh
+++ script/appveyor-build.sh
@@ -6,7 +6,7 @@ set -o xtrace
 echo "MSYSTEM=${MSYSTEM}"
 export VCV_PLUGIN_NAME=DHE-Modules
 export VCV_PLUGIN_DIR="${APPVEYOR_BUILD_FOLDER}"
-export VCV_RACK_DIR="${VCV_PLUGIN_DIR}/../Rack"
+export VCV_RACK_DIR=/tmp/Rack
 export VCV_RACK_COMMIT=master
 
 env | sort
