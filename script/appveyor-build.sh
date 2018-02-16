077ba75af24d3c37fbb11a93aeaebd254532e4bb
Dale Emery <dale@dhemery.com>
2018-02-15 20:17:56 -0800

    Update Dockerfile

--- script/appveyor-build.sh
+++ script/appveyor-build.sh
@@ -8,11 +8,9 @@ export VCV_PLUGIN_NAME=DHE-Modules
 export VCV_PLUGIN_DIR="${APPVEYOR_BUILD_FOLDER}"
 export VCV_RACK_DIR=/c/tmp/Rack
 export VCV_RACK_COMMIT=master
+export VCV_HOME_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 env | sort
 
-cd "$(dirname "$0")"
-. ./build-rack.sh
-
-cd "$(dirname "$0")"
-. ./build-plugin.sh
+. "${VCV_HOME_DIR}/build-rack.sh"
+. "${VCV_HOME_DIR}/build-plugin.sh"
--- script/linux-build.sh
+++ script/linux-build.sh
@@ -3,17 +3,17 @@ set -o errexit
 set -o nounset
 set -o xtrace
 
+export VCV_HOME_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 env | sort
 
-cd "$(dirname "$0")"
-. ./build-rack.sh
+. "${VCV_HOME_DIR}/build-rack.sh"
 
 git clone https://github.com/dhemery/DHE-Modules "${VCV_PLUGIN_DIR}"
 cd "${VCV_PLUGIN_DIR}"
 git checkout "${VCV_PLUGIN_COMMIT}"
 
-cd "$(dirname "$0")"
-. ./build-plugin.sh
+. "${VCV_HOME_DIR}/build-plugin.sh"
 
 mkdir -p ${VCV_INSTALL_DIR}
 cp ${VCV_PLUGIN_DIR}/dist/*.zip ${VCV_INSTALL_DIR}
