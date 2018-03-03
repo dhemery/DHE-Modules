a2800eb7b64b038ce3215c757111b4e0fb9ba994
Dale Emery <dale@dhemery.com>
2018-03-03 13:48:33 -0800

    Draw BS knob labels with jekyll

--- svg-builder/_plugins/filters.rb
+++ svg-builder/_plugins/filters.rb
@@ -1,67 +1,69 @@
 module Jekyll
   module Filters
-    PIXELS_PER_MM = 2.9527559055
-    def to_pixels(mm)
-      mm * PIXELS_PER_MM
+    PX_PER_MM = 2.9527559055
+    def mm_to_px(mm)
+      mm * PX_PER_MM
     end
-    module_function :to_pixels
+    module_function :mm_to_px
 
-    class Port
-      RADIUS = Filters::to_pixels(4.2)
-      DIAMETER = RADIUS*2
+    PORT_DIAMETER = mm_to_px(8.4)
+    LARGE_KNOB_DIAMETER = mm_to_px(13)
+    SMALL_LABEL_FONT_SIZE = 7.0 # px
+    LARGE_LABEL_FONT_SIZE = 9.0 # px
 
-      attr_reader :x, :y, :radius, :diameter
-
-      def initialize(x, y)
+    class RoundControl
+      def initialize(x, y, diameter)
         @x = x
         @y = y
+        @diameter = diameter
+        @radius = diameter / 2.0
       end
 
       def top
-        @y - RADIUS
+        @y - @radius
       end
 
       def left
-        @x - RADIUS
+        @x - @radius
       end
 
       def right
-        @x + RADIUS
+        @x + @radius
       end
 
       def width
-        DIAMETER
+        @diameter
       end
 
       def bottom
-        @y + RADIUS
+        @y + @radius
       end
     end
 
-    class SmallLabel
-      ASCENT_RATIO = 0.66666666666
-      FONT_SIZE = 7.0 # pixels
-      HEIGHT = FONT_SIZE*ASCENT_RATIO
+    class Label
+      ASCENT_RATIO = 0.66666666666 # For Proxima Nova font
 
-      def initialize(center_x, bottom, text, color)
+      def initialize(center_x, bottom, text, color, size)
         @center_x = center_x
         @bottom = bottom
         @text = text
         @color = color
+        @size = size
+        @height = @size * ASCENT_RATIO
       end
 
       def top
-        @bottom - HEIGHT
+        @bottom - @height
       end
 
       def to_liquid
-        %Q[<text text-anchor="middle" fill="#{@color}" x="#{@center_x}" y="#{@bottom}" style="font-family:Proxima Nova;font-weight:bold;font-size:#{FONT_SIZE}px">#{@text}</text>]
+        %Q[<text text-anchor="middle" fill="#{@color}" x="#{@center_x}" y="#{@bottom}" style="font-family:Proxima Nova;font-weight:bold;font-size:#{@size}px">#{@text.upcase}</text>]
       end
     end
 
     class Box
-      STROKE_WIDTH = Filters::to_pixels(0.5)
-      PADDING = Filters::to_pixels(1.0)
+      STROKE_WIDTH = Filters::mm_to_px(0.5)
+      PADDING = Filters::mm_to_px(1.0)
       STROKE_INSET = STROKE_WIDTH / 2
       CORNER_RADIUS = STROKE_WIDTH * 2
 
@@ -87,8 +89,8 @@ module Jekyll
 
     class PortBox
       def initialize(x, y, border, background, label_text, label_color)
-        port = Port.new(x, y)
-        @label = SmallLabel.new(x, port.top - Box::PADDING, label_text, label_color)
+        port = RoundControl.new(x, y, PORT_DIAMETER)
+        @label = Label.new(x, port.top - Box::PADDING, label_text, label_color, SMALL_LABEL_FONT_SIZE)
         @box = Box.new(@label.top, port.right, port.bottom, port.left, border, background)
       end
 
@@ -97,9 +99,26 @@ module Jekyll
       end
     end
 
+    class LabeledRoundControl
+      def initialize(x, y, diameter, text, color, font_size)
+        control = RoundControl.new(x, y, diameter)
+        @label = Label.new(x, control.top - Box::PADDING, text, color, font_size)
+      end
+
+      def to_liquid
+        @label.to_liquid
+      end
+    end
+
+    def labeled_round_control(x_mm, y_mm, diameter, label, color, font_size)
+      x_px = Filters::mm_to_px(x_mm)
+      y_px = Filters::mm_to_px(y_mm)
+      LabeledRoundControl.new(x_px, y_px, diameter, label, color, font_size)
+    end
+
     def port_box(x_mm, y_mm, border, background, label, label_color)
-      x_px = Filters::to_pixels(x_mm)
-      y_px = Filters::to_pixels(y_mm)
+      x_px = Filters::mm_to_px(x_mm)
+      y_px = Filters::mm_to_px(y_mm)
       PortBox.new(x_px, y_px, border, background, label, label_color)
     end
 
@@ -111,8 +130,12 @@ module Jekyll
       port_box(x_mm, y_mm, page["dark_color"], page["light_color"], label, page["dark_color"])
     end
 
-    def out_port_box(page, x, y, label)
-      port_box(x, y, page["dark_color"], page["dark_color"], label, page["light_color"])
+    def out_port_box(page, x_mm, y_mm, label)
+      port_box(x_mm, y_mm, page["dark_color"], page["dark_color"], label, page["light_color"])
+    end
+
+    def labeled_large_knob(page, x_mm, y_mm, label)
+      labeled_round_control(x_mm, y_mm, LARGE_KNOB_DIAMETER, label, page['dark_color'], LARGE_LABEL_FONT_SIZE)
     end
   end
 end
