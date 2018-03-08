263f9b5415d6d156cf959107966d74805021a4ae
Dale Emery <dale@dhemery.com>
2018-03-07 17:09:55 -0800

    Abandon StageProcessor for Hostage

--- src/modules/hostage-module.h
+++ src/modules/hostage-module.h
@@ -8,26 +8,46 @@
 
 namespace DHE {
 
-struct HostageModule : Module, StageProcessor {
-  bool is_sustaining{false};
+struct HostageModule : Module {
+  HostageModule() : Module{PARAMETER_COUNT, INPUT_COUNT, OUTPUT_COUNT},
+                    defer_gate{[this] { return defer_gate_in(); }},
+                    eoc_pulse{1e-3, [this] { return sample_time(); }},
+                    envelope{[this] { return duration_in(); }, [this] { return sample_time(); }},
+                    envelope_gate{[this] { return envelope_gate_in(); }},
+                    phase_0_voltage{[this] { return envelope_in(); }} {
+    defer_gate.on_rising_edge([this] { begin_deferring(); });
+    defer_gate.on_falling_edge([this] { end_deferring(); });
 
-  HostageModule() : Module{PARAMETER_COUNT, INPUT_COUNT, OUTPUT_COUNT}, StageProcessor{} {}
+    envelope_gate.on_rising_edge([this] { on_envelope_gate_rising(); });
+    envelope_gate.on_falling_edge([this] { on_envelope_gate_falling(); });
+  }
 
-  float defer_gate_in() const override {
+  void step() {
+    defer_gate.step();
+    envelope.step();
+    envelope_gate.step();
+    eoc_pulse.step();
+
+    send_active_out();
+    send_eoc_out();
+    send_envelope_out();
+  }
+
+  float defer_gate_in() const {
     return input(DEFER_IN);
   };
 
-  float duration_in() const override {
+  float duration_in() const {
     auto rotation = modulated(DURATION_KNOB, DURATION_CV);
     const auto &range = Duration::range(param(DURATION_SWITCH));
     return Duration::scaled(rotation, range);
   }
 
-  float envelope_gate_in() const override {
+  float envelope_gate_in() const {
     return input(HOLD_GATE_IN);
   }
 
-  float envelope_in() const override {
+  float envelope_in() const {
     return input(ENVELOPE_IN);
   }
 
@@ -35,53 +55,73 @@ struct HostageModule : Module, StageProcessor {
     return param(GATE_MODE_SWITCH) > 0.5f;
   }
 
-  void on_envelope_gate_rising() override {
+  void on_envelope_gate_rising() {
     if (is_in_gate_mode()) {
       begin_sustain();
     } else {
-      StageProcessor::on_envelope_gate_rising();
+      start_envelope();
     }
   }
 
-  void on_envelope_gate_falling() override {
+  void on_envelope_gate_falling() {
     if (is_in_gate_mode()) {
       end_sustain();
     }
-    StageProcessor::on_envelope_gate_falling();
   }
 
-  void send_active_out(bool is_active) override {
-    outputs[ACTIVE_OUT].value = UNIPOLAR_SIGNAL_RANGE.scale(is_active || is_sustaining);
+  void send_active_out() {
+    outputs[ACTIVE_OUT].value = UNIPOLAR_SIGNAL_RANGE.scale(is_active());
   }
 
-  void send_envelope_out(float phase_0_voltage, float ignored) override {
-    outputs[ENVELOPE_OUT].value = phase_0_voltage;
+  void send_envelope_out() {
+    outputs[ENVELOPE_OUT].value = phase_0_voltage.value();
   }
 
-  void send_eoc_out(bool is_pulsing) override {
-    outputs[EOC_OUT].value = UNIPOLAR_SIGNAL_RANGE.scale(is_pulsing);
-  }
-
-  void step() override {
-    StageProcessor::step();
+  void send_eoc_out() {
+    outputs[EOC_OUT].value = UNIPOLAR_SIGNAL_RANGE.scale(eoc_pulse.is_active());
   }
 
   void begin_sustain() {
     envelope.stop();
     phase_0_voltage.hold();
+    is_sustaining = true;
   }
 
   void end_sustain() {
-    eoc_pulse.start();
     is_sustaining = false;
+    eoc_pulse.start();
   }
 
-  enum ParameterIds {
-    DURATION_KNOB,
-    DURATION_SWITCH,
-    GATE_MODE_SWITCH,
-    PARAMETER_COUNT
-  };
+  bool is_active() const {
+    return defer_gate.is_high() || envelope.is_active() || is_sustaining;
+  }
+
+  void begin_deferring() {
+    envelope_gate.suspend_firing();
+    envelope.stop();
+    phase_0_voltage.track();
+  }
+
+  void end_deferring() {
+    phase_0_voltage.hold();
+    envelope_gate.resume_firing();
+  }
+
+  float sample_time() const {
+    return rack::engineGetSampleTime();
+  }
+
+  void start_envelope() {
+    phase_0_voltage.hold();
+    envelope.start();
+  }
+
+  DFlipFlop defer_gate;
+  Ramp eoc_pulse;
+  Ramp envelope;
+  DFlipFlop envelope_gate;
+  bool is_sustaining{false};
+  TrackAndHold phase_0_voltage;
 
   enum InputIds {
     DEFER_IN,
@@ -96,6 +136,13 @@ struct HostageModule : Module, StageProcessor {
     ENVELOPE_OUT,
     EOC_OUT,
     OUTPUT_COUNT
+  };
+
+  enum ParameterIds {
+    DURATION_KNOB,
+    DURATION_SWITCH,
+    GATE_MODE_SWITCH,
+    PARAMETER_COUNT
   };
 };
 
