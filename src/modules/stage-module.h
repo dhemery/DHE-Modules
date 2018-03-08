62c5cb2600d54b4c4cfc746a8e7de09b8a1772fe
Dale Emery <dale@dhemery.com>
2018-03-08 12:47:46 -0800

    Abandon StageProcessor for Stage

--- src/modules/stage-module.h
+++ src/modules/stage-module.h
@@ -3,30 +3,49 @@
 #include <engine.hpp>
 
 #include "module.h"
-#include "stage-processor.h"
 #include "util/controls.h"
+#include "util/d-flip-flop.h"
+#include "util/ramp.h"
 #include "util/range.h"
+#include "util/track-and-hold.h"
 
 namespace DHE {
 
-struct StageModule : public Module, StageProcessor {
+struct StageModule : public Module {
   StageModule()
       : Module(PARAMETER_COUNT, INPUT_COUNT, OUTPUT_COUNT),
-        StageProcessor{} {}
+        defer_gate{[this] { return defer_gate_in(); }},
+        eoc_pulse{1e-3, [this] { return sample_time(); }},
+        envelope{[this] { return duration_in(); }, [this] { return sample_time(); }},
+        envelope_gate{[this] { return envelope_gate_in(); }},
+        phase_0_voltage{[this] { return envelope_in(); }} {
+    defer_gate.on_rising_edge([this] { on_defer_gate_rising(); });
+    defer_gate.on_falling_edge([this] { on_defer_gate_falling(); });
 
-  float defer_gate_in() const override {
+    envelope_gate.on_rising_edge([this] { on_envelope_gate_rising(); });
+
+    envelope.on_completion([this] { on_envelope_completion(); });
+  }
+
+  void begin_deferring() {
+    envelope_gate.suspend_firing();
+    envelope.stop();
+    phase_0_voltage.track();
+  }
+
+  float defer_gate_in() const {
     return input(DEFER_IN);
   }
 
-  float duration_in() const override {
+  float duration_in() const {
     return Duration::scaled(param(DURATION_KNOB), Duration::MEDIUM_RANGE);
   }
 
-  float envelope_gate_in() const override {
+  float envelope_gate_in() const {
     return input(TRIGGER_IN);
   }
 
-  float envelope_in() const override {
+  float envelope_in() const {
     return input(ENVELOPE_IN);
   }
 
@@ -34,29 +53,76 @@ struct StageModule : public Module, StageProcessor {
     return scale(taper(phase), phase_0_voltage, level_in());
   }
 
+  bool is_active() const {
+    return defer_gate.is_high() || envelope.is_active();
+  }
+
   float level_in() const {
     return Level::scaled(param(LEVEL_KNOB));
   }
 
+  void on_defer_gate_falling() {
+    stop_deferring();
+  }
+
+  void on_defer_gate_rising() {
+    begin_deferring();
+  }
+
+  void on_envelope_completion() {
+    eoc_pulse.start();
+  }
+
+  void on_envelope_gate_rising() {
+    start_envelope();
+  }
+
+  float sample_time() const {
+    return rack::engineGetSampleTime();
+  }
+
+  void send_active_out(bool is_active) {
+    outputs[ACTIVE_OUT].value = UNIPOLAR_SIGNAL_RANGE.scale(is_active);
+  }
+
+  void send_envelope_out(float phase_0_voltage, float phase) {
+    outputs[ENVELOPE_OUT].value = envelope_out(phase_0_voltage, phase);
+  }
+
+  void send_eoc_out(bool is_pulsing) {
+    outputs[EOC_OUT].value = UNIPOLAR_SIGNAL_RANGE.scale(is_pulsing);
+  }
+
+  void start_envelope() {
+    phase_0_voltage.hold();
+    envelope.start();
+  }
+
   void step() override {
-    StageProcessor::step();
+    defer_gate.step();
+    envelope.step();
+    envelope_gate.step();
+    eoc_pulse.step();
+
+    send_active_out(is_active());
+    send_eoc_out(eoc_pulse.is_active());
+    send_envelope_out(phase_0_voltage.value(), envelope.phase());
+  }
+
+  void stop_deferring() {
+    phase_0_voltage.hold();
+    envelope_gate.resume_firing();
   }
 
   float taper(float phase) const {
     return Taper::j(phase, param(CURVE_KNOB));
   }
 
-  void send_active_out(bool is_active) override {
-    outputs[ACTIVE_OUT].value = UNIPOLAR_SIGNAL_RANGE.scale(is_active);
-  }
-
-  void send_envelope_out(float phase_0_voltage, float phase) override {
-    outputs[ENVELOPE_OUT].value = envelope_out(phase_0_voltage, phase);
-  }
-
-  void send_eoc_out(bool is_pulsing) override {
-    outputs[EOC_OUT].value = UNIPOLAR_SIGNAL_RANGE.scale(is_pulsing);
-  }
+  DFlipFlop defer_gate;
+  Ramp eoc_pulse;
+  Ramp envelope;
+  DFlipFlop envelope_gate;
+  TrackAndHold phase_0_voltage;
 
   enum ParameterIIds {
     DURATION_KNOB,
