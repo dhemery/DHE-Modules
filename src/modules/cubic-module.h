cb7cbc668a5622dd01b3663cd3f0c1638a363769
Dale Emery <dale@dhemery.com>
2018-03-31 14:50:24 -0700

    Cubic bx^2 (squared) term

--- src/modules/cubic-module.h
+++ src/modules/cubic-module.h
@@ -12,18 +12,30 @@ struct CubicModule : Module {
     return coefficient_knob_range.scale(rotation);
   }
 
+  float offset() {
+    return BIPOLAR_SIGNAL_RANGE.scale(param(OFFSET_KNOB));
+  }
+
+  float scaled(float x) {
+    return x * coefficient(param(SCALE_KNOB));
+  }
+
+  float squared(float x) {
+    auto normalized = x / 5.f;
+    auto squared = normalized * normalized;
+    return 5.f * squared * coefficient(param(SQUARED_KNOB));
+  }
+
   void step() override {
     auto x = input(X);
-    auto offset = BIPOLAR_SIGNAL_RANGE.scale(param(OFFSET_KNOB));
-    auto x_scaled = x * coefficient(param(SCALE_KNOB));
-    outputs[Y].value = x_scaled + offset;
+    outputs[Y].value = squared(x) + scaled(x) + offset();
   }
 
   enum ParameterIds {
     OFFSET_KNOB,
     SCALE_KNOB,
-    SQUARED,
-    CUBED,
+    SQUARED_KNOB,
+    CUBED_KNOB,
     PARAMETER_COUNT
   };
   enum InputIds {
--- test/cubic-tests.cpp
+++ test/cubic-tests.cpp
@@ -6,12 +6,14 @@ TEST_CASE("Cubic Module") {
   DHE::CubicModule module{};
   auto &offset_knob = module.params[module.OFFSET_KNOB].value;
   auto &scale_knob = module.params[module.SCALE_KNOB].value;
+  auto &squared_knob = module.params[module.SQUARED_KNOB].value;
 
   auto &x = module.inputs[module.X].value;
   auto &y = module.outputs[module.Y].value;
 
-  offset_knob = 0.5f; // Default dx_0 term yields 0
-  scale_knob = 0.75f; // Default cx^1 term yeilds x
+  squared_knob = 0.5f;  // default b = 0, so bx^2 = 0
+  scale_knob = 0.75f;   // default c = 1, so cx^1 = x
+  offset_knob = 0.5f;   // default d = 0, so dx_0 = 0
 
   SECTION("offset") {
     x = 1.234f;
@@ -68,6 +70,49 @@ TEST_CASE("Cubic Module") {
       scale_knob = 0.f;
       module.step();
       REQUIRE_THAT(y, near(x * -2.f));
+    }
+  }
+
+  SECTION("squared") {
+    scale_knob = 0.5f; // no x^1 term
+
+    SECTION("max rotation yields 2(x^2)") {
+      x = 4.872f;
+      auto bipolar_squared_x = pow(x / 5.f, 2.f) * 5.f;
+      squared_knob = 1.f;
+      module.step();
+      REQUIRE_THAT(y, near(bipolar_squared_x * 2.f));
+    }
+
+    SECTION("three-quarter rotation yields x^2") {
+      x = 3.872f;
+      auto bipolar_squared_x = pow(x / 5.f, 2.f) * 5.f;
+      squared_knob = 0.75f;
+      module.step();
+      REQUIRE_THAT(y, near(bipolar_squared_x));
+    }
+
+    SECTION("central rotation yields 0") {
+      x = 3.872f;
+      squared_knob = 0.5f;
+      module.step();
+      REQUIRE_THAT(y, near(0.f));
+    }
+
+    SECTION("one-quarter rotation yields -(x^2)") {
+      x = 1.367f;
+      auto bipolar_squared_x = pow(x / 5.f, 2.f) * 5.f;
+      squared_knob = 0.25f;
+      module.step();
+      REQUIRE_THAT(y, near(-bipolar_squared_x));
+    }
+
+    SECTION("min rotation yields -2(x^2)") {
+      x = 4.872f;
+      auto bipolar_squared_x = pow(x / 5.f, 2.f) * 5.f;
+      squared_knob = 0.f;
+      module.step();
+      REQUIRE_THAT(y, near(bipolar_squared_x * -2.f));
     }
   }
 }
