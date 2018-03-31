2eac663cb11b2c296edfa4d93b98f1ee7f5a249d
Dale Emery <dale@dhemery.com>
2018-03-31 15:56:47 -0700

    Fix bad type in near() signature

--- test/lib/matchers.h
+++ test/lib/matchers.h
@@ -2,7 +2,7 @@
 
 #include "lib/catch.hpp"
 
-inline Catch::Matchers::Floating::WithinUlpsMatcher near(float expected, float proximity = 1) {
+inline Catch::Matchers::Floating::WithinUlpsMatcher near(float expected, int proximity = 1) {
   return Catch::Matchers::WithinULP(expected, proximity);
 }
 
