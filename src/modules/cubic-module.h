dc73f3bb2e18832634d73ac0696540c2bbfa3d01
Dale Emery <dale@dhemery.com>
2018-03-31 14:22:55 -0700

    Cubic cx^1 (scale) term

--- src/modules/cubic-module.h
+++ src/modules/cubic-module.h
@@ -7,19 +7,31 @@ struct CubicModule : Module {
   CubicModule() : Module{PARAMETER_COUNT, INPUT_COUNT, OUTPUT_COUNT} {
   }
 
+  float coefficient(float rotation) {
+    static Range coefficient_knob_range = Range{-2.0f, 2.0f};
+    return coefficient_knob_range.scale(rotation);
+  }
+
   void step() override {
-    outputs[OUT].value = BIPOLAR_SIGNAL_RANGE.scale(param(CONSTANT));
+    auto x = input(X);
+    auto offset = BIPOLAR_SIGNAL_RANGE.scale(param(OFFSET_KNOB));
+    auto x_scaled = x * coefficient(param(SCALE_KNOB));
+    outputs[Y].value = x_scaled + offset;
   }
 
   enum ParameterIds {
-    CONSTANT,
+    OFFSET_KNOB,
+    SCALE_KNOB,
+    SQUARED,
+    CUBED,
     PARAMETER_COUNT
   };
   enum InputIds {
+    X,
     INPUT_COUNT
   };
   enum OutputIds {
-    OUT,
+    Y,
     OUTPUT_COUNT
   };
 };
--- test/cubic-tests.cpp
+++ test/cubic-tests.cpp
@@ -4,26 +4,70 @@
 
 TEST_CASE("Cubic Module") {
   DHE::CubicModule module{};
-  auto &offset_knob = module.params[module.CONSTANT].value;
-  auto &out = module.outputs[module.OUT].value;
+  auto &offset_knob = module.params[module.OFFSET_KNOB].value;
+  auto &scale_knob = module.params[module.SCALE_KNOB].value;
+
+  auto &x = module.inputs[module.X].value;
+  auto &y = module.outputs[module.Y].value;
+
+  offset_knob = 0.5f; // Default dx_0 term yields 0
+  scale_knob = 0.75f; // Default cx^1 term yeilds x
 
   SECTION("offset") {
-    SECTION("max rotation yields max bipolar signal") {
+    x = 1.234f;
+    SECTION("max rotation adds max bipolar signal") {
       offset_knob = 1.f;
       module.step();
-      REQUIRE_THAT(out, near(DHE::BIPOLAR_SIGNAL_RANGE.upper_bound));
+      REQUIRE_THAT(y, near(x + DHE::BIPOLAR_SIGNAL_RANGE.upper_bound));
     }
 
-    SECTION("min rotation yields max bipolar signal") {
+    SECTION("min rotation adds min bipolar signal") {
       offset_knob = 0.f;
       module.step();
-      REQUIRE_THAT(out, near(DHE::BIPOLAR_SIGNAL_RANGE.lower_bound));
+      REQUIRE_THAT(y, near(x + DHE::BIPOLAR_SIGNAL_RANGE.lower_bound));
     }
 
-    SECTION("central rotation yields central bipolar signal") {
+    SECTION("central rotation adds 0") {
       offset_knob = 0.5f;
       module.step();
-      REQUIRE_THAT(out, near(DHE::BIPOLAR_SIGNAL_RANGE.scale(0.5f)));
+      REQUIRE_THAT(y, near(x + 0.f));
+    }
+  }
+
+  SECTION("scale") {
+    SECTION("max rotation yields 2x") {
+      x = 2.181f;
+      scale_knob = 1.f;
+      module.step();
+      REQUIRE_THAT(y, near(x * 2.f));
+    }
+
+    SECTION("three-quarter rotation yields x") {
+      x = 3.345;
+      scale_knob = 0.75f;
+      module.step();
+      REQUIRE_THAT(y, near(x));
+    }
+
+    SECTION("central rotation yields 0") {
+      x = 2.847;
+      scale_knob = 0.5f;
+      module.step();
+      REQUIRE_THAT(y, near(0.f));
+    }
+
+    SECTION("quarter rotation yields -x") {
+      x = 4.394;
+      scale_knob = 0.25f;
+      module.step();
+      REQUIRE_THAT(y, near(-x));
+    }
+
+    SECTION("min rotation yields -2x") {
+      x = 4.394;
+      scale_knob = 0.f;
+      module.step();
+      REQUIRE_THAT(y, near(x * -2.f));
     }
   }
 }
