6141b541c21385a290edc51992e913521cd12988
Dale Emery <dale@dhemery.com>
2018-04-04 15:10:22 -0700

    Cubic input gain test update

--- test/cubic-tests.cpp
+++ test/cubic-tests.cpp
@@ -34,16 +34,14 @@ TEST_CASE("Cubic Module") {
   auto x1_coefficient = Scaled{x1_knob, DHE::CubicModule::coefficient_range()};
   auto x0_coefficient = Scaled{x0_knob, DHE::CubicModule::coefficient_range()};
 
-  auto input_gain = Scaled{input_gain_knob, DHE::CubicModule::gain_range()};
   auto output_gain = Scaled{output_gain_knob, DHE::CubicModule::gain_range()};
 
-  x3_coefficient = 0.f;
-  x2_coefficient = 0.f;
-  x1_coefficient = 0.f;
-  x0_coefficient = 0.f;
-
-  input_gain = 1.f;
-  output_gain = 1.f;
+  x3_knob = 0.5f; // 0x^3
+  x2_knob = 0.5f; // 0x^2
+  x1_knob = 0.5f; // 0x^1
+  x0_knob = 0.5f; // 0x^0
+  input_gain_knob = 0.5f; // input gain = 1
+  output_gain_knob = 0.5f; // output gain = 1
 
   SECTION("ax^3") {
     auto x = -0.5f; // Arbitrary-ish
@@ -262,40 +260,53 @@ TEST_CASE("Cubic Module") {
   }
 
   SECTION("input gain") {
-    x3_coefficient = 1.340f;
-    x2_coefficient = 0.482f;
-    x1_coefficient = -1.929f;
-    x0_coefficient = -0.283f;
-    auto x = 0.849f;
+    auto x = -0.5f; // Arbitrary-ish
+    // Arbitrary f(x): y = -2x^3 - x^2 + x + 2
+    auto a = -2.f;
+    auto b = -1.f;
+    auto c = 1.f;
+    auto d = 2.f;
+    // Dial in f(x)
+    x3_knob = 0.f;    // -2x^3
+    x2_knob = 0.25f;  // -x^2
+    x1_knob = 0.75f;  // x
+    x0_knob = 1.f;    // 2
 
-    SECTION("0 gives f(0)") {
-      input_gain = 0.f;
+
+    SECTION("0% rotation -> f(0x)") {
+      input_gain_knob = 0.f;
+      auto input_gain = 0.f;
+      auto xg = x*input_gain;
+      auto y = a*xg*xg*xg + b*xg*xg + c*xg + d;
+
       input_signal = x*5.f;
       cubic.step();
 
-      auto xg = x*input_gain();
-      auto y = x3_coefficient()*xg*xg*xg + x2_coefficient()*xg*xg + x1_coefficient()*xg + x0_coefficient();
-      REQUIRE(output_signal==Approx(y*5.f));
+      REQUIRE(output_signal==5.f*y);
     }
 
-    SECTION("1 gives f(x)") {
-      input_gain = 1.f;
+    SECTION("50% rotation -> f(1x)") {
+      input_gain_knob = 0.f;
+      auto input_gain = 0.f;
+      auto xg = x*input_gain;
+      auto y = a*xg*xg*xg + b*xg*xg + c*xg + d;
+
       input_signal = x*5.f;
       cubic.step();
 
-      auto xg = x*input_gain();
-      auto y = x3_coefficient()*xg*xg*xg + x2_coefficient()*xg*xg + x1_coefficient()*xg + x0_coefficient();
-      REQUIRE(output_signal==Approx(y*5.f));
+      REQUIRE(output_signal==5.f*y);
     }
 
-    SECTION("2 gives f(2x)") {
-      input_gain = 2.f;
+    SECTION("100% rotation -> f(2x)") {
+      input_gain_knob = 0.f;
+      auto input_gain = 0.f;
+      auto xg = x*input_gain;
+      auto y = a*xg*xg*xg + b*xg*xg + c*xg + d;
+
       input_signal = x*5.f;
       cubic.step();
 
-      auto xg = x*input_gain();
-      auto y = x3_coefficient()*xg*xg*xg + x2_coefficient()*xg*xg + x1_coefficient()*xg + x0_coefficient();
-      REQUIRE(output_signal==Approx(y*5.f));
+      REQUIRE(output_signal==5.f*y);
     }
   }
 
