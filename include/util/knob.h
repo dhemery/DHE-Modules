7f5c6b0707611b21edbdda135b9eb54b93ed97da
Dale Emery <dale@dhemery.com>
2018-11-25 02:13:53 -0800

    Clean up Knob

--- include/util/knob.h
+++ include/util/knob.h
@@ -9,27 +9,31 @@ namespace DHE {
 class Knob {
 public:
   static auto plain(const rack::Module *module, int knob) -> Knob {
-    auto supplier = [module, knob] { return module->params[knob].value; };
-    return Knob{supplier};
+    const auto *knob_param = &module->params[knob];
+    return Knob{[knob_param] { return knob_param->value; }};
   }
 
   static auto modulated(const rack::Module *module, int knob, int cv) -> Knob {
-    auto supplier = [module, knob, cv] {
-      auto rotation = module->params[knob].value;
-      auto offset = module->inputs[cv].value*cv_to_offset;
-      return rotation + offset;
-    };
-    return Knob{supplier};
+    const auto *knob_param = &module->params[knob];
+    const auto *cv_input = &module->inputs[cv];
+    return Knob{[knob_param, cv_input] {
+      return modulated(knob_param->value, cv_input->value);
+    }};
   }
 
   static auto modulated(const rack::Module *module, int knob, int cv, int av) -> Knob {
-    auto supplier = [module, knob, cv, av] {
-      auto rotation = module->params[knob].value;
-      auto offset = module->inputs[cv].value*cv_to_offset;
-      auto multipler = av_range.scale(module->params[av].value);
-      return rotation + multipler*offset;
-    };
-    return Knob{supplier};
+    const auto *knob_param = &module->params[knob];
+    const auto *cv_input = &module->inputs[cv];
+    const auto *av_param = &module->params[av];
+    return Knob{[knob_param, cv_input, av_param] {
+      return modulated(knob_param->value, cv_input->value, av_param->value);
+    }};
+  }
+
+  static auto modulated(float rotation, float cv = 0.f, float av = 1.f) -> float {
+    auto offset = cv*cv_to_offset;
+    auto multipler = av_range.scale(av);
+    return rotation + multipler*offset;
   }
 
   auto operator()() const -> float {
