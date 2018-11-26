9bb95b8613094e6371ada01f113530c348c6fdd3
Dale Emery <dale@dhemery.com>
2018-11-25 22:04:36 -0800

    Clean up knob

--- include/controls/knob.h
+++ include/controls/knob.h
@@ -13,28 +13,29 @@ public:
   template <typename Supplier>
   explicit Knob(const Supplier &supplier) : supplier{supplier} {}
 
-  static auto plain(const rack::Module *module, int knob) -> Knob {
-    const auto *knob_param = &module->params[knob];
+  static auto plain(const rack::Module *module, int knob_index) -> Knob {
+    const auto *knob_param = &module->params[knob_index];
     return Knob{[knob_param] { return knob_param->value; }};
   }
 
-  static auto with_cv(const rack::Module *module, int knob, int cv) -> Knob {
-    const auto *knob_param = &module->params[knob];
-    const auto *cv_input = &module->inputs[cv];
-    return Knob{[knob_param, cv_input] {
-      return modulate(knob_param->value, cv_input->value);
-    }};
+  static auto with_cv(const rack::Module *module, int knob_index, int cv_index)
+      -> Knob {
+    const auto *knob_param = &module->params[knob_index];
+    const auto modulate = modulated_by(module, cv_index);
+    return Knob{[knob_param, modulate] { return modulate(knob_param->value); }};
   }
 
-  static auto with_av(const rack::Module *module, int knob, int cv, int av)
-      -> Knob {
-    const auto *knob_param = &module->params[knob];
-    const auto *cv_input = &module->inputs[cv];
-    const auto *av_param = &module->params[av];
-    return Knob{[knob_param, cv_input, av_param] {
-      return modulate(knob_param->value, cv_input->value, av_param->value);
-    }};
+  static auto with_av(const rack::Module *module, int knob_index, int cv_index,
+                      int av_index) -> Knob {
+    const auto *knob_param = &module->params[knob_index];
+    const auto modulate = modulated_by(module, cv_index, av_index);
+    return Knob{[knob_param, modulate] { return modulate(knob_param->value); }};
   }
+
+  auto operator()() const -> float { return supplier(); }
+
+private:
+  const std::function<float()> supplier;
 
   static auto modulate(float rotation, float cv = 0.f, float av = 1.f)
       -> float {
@@ -46,9 +47,21 @@ public:
     return rotation + multipler * offset;
   }
 
-  auto operator()() const -> float { return supplier(); }
+  static auto modulated_by(const rack::Module *module, int cv_index)
+      -> const std::function<float(float)> {
+    const auto *cv_input = &module->inputs[cv_index];
+    return [cv_input](float rotation) -> float {
+      return modulate(rotation, cv_input->value);
+    };
+  }
 
-private:
-  const std::function<float()> supplier;
+  static auto modulated_by(const rack::Module *module, int cv_index,
+                           int av_index) -> const std::function<float(float)> {
+    const auto *cv_input = &module->inputs[cv_index];
+    const auto *av_param = &module->params[av_index];
+    return [cv_input, av_param](float rotation) -> float {
+      return modulate(rotation, cv_input->value, av_param->value);
+    };
+  }
 };
 } // namespace DHE
