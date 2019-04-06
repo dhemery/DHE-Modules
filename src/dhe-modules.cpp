894d7c2fe316a0ba02be46f1664dfc32e9ae69ba
Dale Emery <dale@dhemery.com>
2019-04-06 15:31:00 -0700

    Add registerModel() to dhe-modles.cpp

--- src/dhe-modules.cpp
+++ src/dhe-modules.cpp
@@ -1,10 +1,4 @@
-//namespace rack {
-//struct Plugin;
-//struct Model;
-//} // namespace rack
-
 #include "plugin.hpp"
-#include "util/common.hpp"
 
 #include "modules/Cubic.h"
 #include "panels/CubicPanel.h"
@@ -21,27 +15,32 @@ extern rack::Model *modelTapers;
 extern rack::Model *modelUpstage;
 extern rack::Model *modelXycloid;
 
-rack::Model *modelCubic = rack::Model::create<DHE::Cubic, DHE::CubicPanel>(
-    "DHE-Modules", "Cubic", "Cubic", rack::FUNCTION_GENERATOR_TAG);
+rack::Plugin *plugin;
+
+template<typename TModule, typename TPanel, typename... TTag>
+void registerModel(const std::string& name, TTag... tags) {
+  rack::Model *model = rack::Model::create<TModule, TPanel, TTag...>("DHE-Modules", name, name, tags...);
+  plugin->addModel(model);
+}
 
 void init(rack::Plugin *p) {
   plugin = p;
 
-  p->slug = "DHE-Modules";
-  p->version = TOSTRING(VERSION);
+  plugin->slug = "DHE-Modules";
+  plugin->version = TOSTRING(VERSION);
 
-  p->addModel(modelBlossom);
-  p->addModel(modelBoosterStage);
-  p->addModel(modelCubic);
-  p->addModel(modelFunc);
-  p->addModel(modelFunc6);
-  p->addModel(modelHostage);
-  p->addModel(modelRanger);
-  p->addModel(modelStage);
-  p->addModel(modelSwave);
-  p->addModel(modelTapers);
-  p->addModel(modelUpstage);
-  p->addModel(modelXycloid);
+  plugin->addModel(modelBlossom);
+  plugin->addModel(modelBoosterStage);
+
+  registerModel<DHE::Cubic, DHE::CubicPanel>("Cubic", rack::FUNCTION_GENERATOR_TAG);
+
+  plugin->addModel(modelFunc);
+  plugin->addModel(modelFunc6);
+  plugin->addModel(modelHostage);
+  plugin->addModel(modelRanger);
+  plugin->addModel(modelStage);
+  plugin->addModel(modelSwave);
+  plugin->addModel(modelTapers);
+  plugin->addModel(modelUpstage);
+  plugin->addModel(modelXycloid);
 }
-
-rack::Plugin *plugin;
