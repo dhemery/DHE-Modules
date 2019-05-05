beb03a46e601924a8422a040c128e89233505ab8
Dale Emery <dale@dhemery.com>
2019-05-05 09:35:24 -0700

    Config Ranger params

--- src/modules/Ranger.cpp
+++ src/modules/Ranger.cpp
@@ -3,7 +3,20 @@
 
 namespace DHE {
 
-Ranger::Ranger() { config(PARAMETER_COUNT, INPUT_COUNT, OUTPUT_COUNT); }
+Ranger::Ranger() {
+  config(PARAMETER_COUNT, INPUT_COUNT, OUTPUT_COUNT);
+
+  configKnob(LEVEL_KNOB, "Level");
+  configCvGain(LEVEL_AV, "Level");
+
+  configKnob(LIMIT_1_KNOB, "Limit 1");
+  configCvGain(LIMIT_1_AV, "Limit 1");
+  configSignalRange(LIMIT_1_RANGE_SWITCH, "Limit 1", false);
+
+  configKnob(LIMIT_2_KNOB, "Limit 2");
+  configCvGain(LIMIT_2_AV, "Limit 2");
+  configSignalRange(LIMIT_2_RANGE_SWITCH, "Limit 2", false);
+}
 
 auto Ranger::level() -> float {
   return modulated(LEVEL_KNOB, LEVEL_CV, LEVEL_AV);
