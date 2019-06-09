4af8434cc0e7b59d7a48cf073c8b69788d94a29a
Dale Emery <dale@dhemery.com>
2019-06-09 11:34:22 -0700

    Restore duration tests

--- test/DurationTests.cpp
+++ test/DurationTests.cpp
@@ -1,96 +1,105 @@
-//#include "modules/controls/Duration.h"
-//
-//#include <gtest/gtest.h>
-//
-//namespace dhe {
-//
-//struct DurationTest : public ::testing::Test {
-//  // tolerance = 6 decimal places
-//  static constexpr float tolerance = 0.000001f;
-//
-//  Range const *range;
-//  float rotation;
-//
-//  std::function<float()> getRotation{[this]() -> float { return rotation; }};
-//
-//  std::function<Range const *()> getRange{
-//      [this]() -> Range const * { return range; }};
-//
-////  std::function<float()> duration = Duration::from(getRotation, getRange);
-//  std::function<float()> duration = []() -> float { return 0.f; };
-//};
-//
-//TEST_F(DurationTest, shortRange_minimumDuration_isShortRangeLowerBound) {
-//  range = &duration::shortRange;
-//  rotation = 0.f;
-//
-//  const auto expected = duration::shortRange.lower_bound;
-//  ASSERT_NEAR(duration(), expected, expected * tolerance);
-//}
-//
-//TEST_F(DurationTest,
-//       shortRange_middleDuration_isOneTenthOfShortRangeUpperBound) {
-//  range = &duration::shortRange;
-//  rotation = 0.5f;
-//
-//  const auto expected = duration::shortRange.upper_bound / 10.f;
-//  ASSERT_NEAR(duration(), expected, expected * tolerance);
-//}
-//
-//TEST_F(DurationTest, shortRange_maximumDuration_isShortRangeUpperBound) {
-//  range = &duration::shortRange;
-//  rotation = 1.f;
-//
-//  const auto expected = duration::shortRange.upper_bound;
-//  ASSERT_NEAR(duration(), expected, expected * tolerance);
-//}
-//
-//TEST_F(DurationTest, mediumRange_minimumDuration_isMediumRangeLowerBound) {
-//  range = &duration::mediumRange;
-//  rotation = 0.f;
-//
-//  const auto expected = duration::mediumRange.lower_bound;
-//  ASSERT_NEAR(duration(), expected, expected * tolerance);
-//}
-//
-//TEST_F(DurationTest,
-//       mediumRange_middleDuration_isOneTenthOfMediumRangeUpperBound) {
-//  range = &duration::mediumRange;
-//  rotation = 0.5f;
-//
-//  const auto expected = duration::mediumRange.upper_bound / 10.f;
-//  ASSERT_NEAR(duration(), expected, expected * tolerance);
-//}
-//
-//TEST_F(DurationTest, mediumRange_maximumDuration_isMediumRangeUpperBound) {
-//  range = &duration::mediumRange;
-//  rotation = 1.f;
-//
-//  const auto expected = duration::mediumRange.upper_bound;
-//  ASSERT_NEAR(duration(), expected, expected * tolerance);
-//}
-//
-//TEST_F(DurationTest, longRange_minimumDuration_isLongRangeLowerBound) {
-//  range = &duration::longRange;
-//  rotation = 0.f;
-//
-//  const auto expected = duration::longRange.lower_bound;
-//  ASSERT_NEAR(duration(), expected, expected * tolerance);
-//}
-//
-//TEST_F(DurationTest, longRange_middleDuration_isOneTenthOfLongRangeUpperBound) {
-//  range = &duration::longRange;
-//  rotation = 0.5f;
-//
-//  const auto expected = duration::longRange.upper_bound / 10.f;
-//  ASSERT_NEAR(duration(), expected, expected * tolerance);
-//}
-//
-//TEST_F(DurationTest, longRange_maximumDuration_isLongRangeUpperBound) {
-//  range = &duration::longRange;
-//  rotation = 1.f;
-//
-//  const auto expected = duration::longRange.upper_bound;
-//  ASSERT_NEAR(duration(), expected, expected * tolerance);
-//}
-//} // namespace DHE
+#include "modules/controls/DurationInputs.h"
+
+#include <gtest/gtest.h>
+
+namespace dhe {
+
+struct DurationTest : public ::testing::Test {
+  // tolerance = 6 decimal places
+  static constexpr float tolerance = 0.000001f;
+  rack::engine::Module module{};
+  DurationTest() { module.config(1, 1, 0); }
+
+  void setRotation(float rotation) { module.params[0].setValue(rotation); }
+};
+
+TEST_F(DurationTest, shortRange_minimumDuration_isShortRangeLowerBound) {
+  auto const range = shortDurationRange;
+  setRotation(0.F);
+
+  auto const duration = dhe::duration(&module, 0, range);
+
+  auto const expected = range.lowerBound;
+  ASSERT_NEAR(duration, expected, expected * tolerance);
+}
+
+TEST_F(DurationTest, shortRange_middleDuration_isOneTenthOfShortRangeUpperBound) {
+  auto const range = shortDurationRange;
+  setRotation(0.5F);
+
+  auto const duration = dhe::duration(&module, 0, range);
+
+  auto const expected = range.upperBound * 0.1F;
+  ASSERT_NEAR(duration, expected, expected * tolerance);
+}
+
+TEST_F(DurationTest, shortRange_maximumDuration_isShortRangeUpperBound) {
+  auto const range = shortDurationRange;
+  setRotation(1.F);
+
+  auto const duration = dhe::duration(&module, 0, range);
+
+  auto const expected = range.upperBound;
+  ASSERT_NEAR(duration, expected, expected * tolerance);
+}
+
+TEST_F(DurationTest, mediumRange_minimumDuration_isMediumRangeLowerBound) {
+  auto const range = mediumDurationRange;
+  setRotation(0.F);
+
+  auto const duration = dhe::duration(&module, 0, range);
+
+  auto const expected = range.lowerBound;
+  ASSERT_NEAR(duration, expected, expected * tolerance);
+}
+
+TEST_F(DurationTest, mediumRange_middleDuration_isOneTenthOfMediumRangeUpperBound) {
+  auto const range = mediumDurationRange;
+  setRotation(0.5F);
+
+  auto const duration = dhe::duration(&module, 0, range);
+
+  auto const expected = range.upperBound * 0.1F;
+  ASSERT_NEAR(duration, expected, expected * tolerance);
+}
+
+TEST_F(DurationTest, mediumRange_maximumDuration_isMediumRangeUpperBound) {
+  auto const range = mediumDurationRange;
+  setRotation(1.F);
+
+  auto const duration = dhe::duration(&module, 0, range);
+
+  auto const expected = range.upperBound;
+  ASSERT_NEAR(duration, expected, expected * tolerance);
+}
+
+TEST_F(DurationTest, longRange_minimumDuration_isLongRangeLowerBound) {
+  auto const range = longDurationRange;
+  setRotation(0.F);
+
+  auto const duration = dhe::duration(&module, 0, range);
+
+  auto const expected = range.lowerBound;
+  ASSERT_NEAR(duration, expected, expected * tolerance);
+}
+
+TEST_F(DurationTest, longRange_middleDuration_isOneTenthOfLongRangeUpperBound) {
+  auto const range = longDurationRange;
+  setRotation(0.5F);
+
+  auto const duration = dhe::duration(&module, 0, range);
+
+  auto const expected = range.upperBound * 0.1F;
+  ASSERT_NEAR(duration, expected, expected * tolerance);
+}
+
+TEST_F(DurationTest, longRange_maximumDuration_isLongRangeUpperBound) {
+  auto const range = longDurationRange;
+  setRotation(1.F);
+
+  auto const duration = dhe::duration(&module, 0, range);
+
+  auto const expected = range.upperBound;
+  ASSERT_NEAR(duration, expected, expected * tolerance);
+}
+} // namespace dhe
