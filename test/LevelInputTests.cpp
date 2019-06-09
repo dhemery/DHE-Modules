b9678cbf6e09127f74cc5982124cc921f37ddd43
Dale Emery <dale@dhemery.com>
2019-06-09 11:54:21 -0700

    Restore level input tests

---
+++ test/LevelInputTests.cpp
@@ -0,0 +1,76 @@
+
+#include "modules/controls/LevelInputs.h"
+
+#include <cmath>
+#include <gtest/gtest.h>
+
+namespace dhe {
+
+struct LevelInputTest : public ::testing::Test {
+  // tolerance = 6 decimal places
+  static constexpr float tolerance = 0.000001F;
+  static constexpr int levelKnob = 0;
+
+  rack::engine::Module module{};
+  LevelInputTest() { module.config(1, 1, 0); }
+
+  void setParam(int param, float value) { module.params[param].setValue(value); }
+};
+
+TEST_F(LevelInputTest, bipolarRange_minimumLevel_isBipolarRangeLowerBound) {
+  setParam(levelKnob, 0.F);
+
+  auto const level = dhe::level(&module, levelKnob, bipolarSignalRange);
+
+  auto const expected = bipolarSignalRange.lowerBound;
+  ASSERT_NEAR(level, expected, std::abs(expected * tolerance));
+}
+
+TEST_F(LevelInputTest, bipolarRange_middleLevel_isMidpointOfBipolarRange) {
+  setParam(levelKnob, 0.5F);
+
+  auto const level = dhe::level(&module, levelKnob, bipolarSignalRange);
+
+  auto const expected
+      = (bipolarSignalRange.upperBound - bipolarSignalRange.lowerBound) * 0.5 + bipolarSignalRange.lowerBound;
+  ASSERT_NEAR(level, expected, std::abs(expected * tolerance));
+}
+
+TEST_F(LevelInputTest, bipolarRange_maximumLevel_isBipolarRangeUpperBound) {
+  setParam(levelKnob, 1.F);
+
+  auto const level = dhe::level(&module, levelKnob, bipolarSignalRange);
+
+  auto const expected = bipolarSignalRange.upperBound;
+  ASSERT_NEAR(level, expected, std::abs(expected * tolerance));
+}
+
+TEST_F(LevelInputTest, UnipolarRange_minimumLevel_isUnipolarRangeLowerBound) {
+  setParam(levelKnob, 0.F);
+
+  auto const level = dhe::level(&module, levelKnob, unipolarSignalRange);
+
+  auto const expected = unipolarSignalRange.lowerBound;
+  ASSERT_NEAR(level, expected, std::abs(expected * tolerance));
+}
+
+TEST_F(LevelInputTest, UnipolarRange_middleLevel_isMidpointOfUnipolarRange) {
+  setParam(levelKnob, 0.5F);
+
+  auto const level = dhe::level(&module, levelKnob, unipolarSignalRange);
+
+  auto const expected
+      = (unipolarSignalRange.upperBound - unipolarSignalRange.lowerBound) * 0.5 + unipolarSignalRange.lowerBound;
+  ASSERT_NEAR(level, expected, std::abs(expected * tolerance));
+}
+
+TEST_F(LevelInputTest, UnipolarRange_maximumLevel_isUnipolarRangeUpperBound) {
+  setParam(levelKnob, 1.F);
+
+  auto const level = dhe::level(&module, levelKnob, unipolarSignalRange);
+
+  auto const expected = unipolarSignalRange.upperBound;
+  ASSERT_NEAR(level, expected, std::abs(expected * tolerance));
+}
+
+} // namespace dhe
