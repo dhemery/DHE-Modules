f28308ba2f346ee62f468dc7ba91295ac99c03c1
Dale Emery <dale@dhemery.com>
2019-06-21 00:40:48 -0700

    Gator fully functional

--- include/modules/Gator.h
+++ include/modules/Gator.h
@@ -4,10 +4,21 @@
 namespace dhe {
 
 class Gator : public rack::engine::Module {
-
 public:
   Gator();
   void process(const ProcessArgs &args) override;
+
+  void setAllOutputsFalse() {
+    for (int i = 0; i < OutputCount; i++) {
+      outputs[i].setVoltage(0.F);
+    }
+  }
+
+  void setOutputs(int outputId, int negatedOutputId, bool value) {
+    auto const voltage = value ? 10.F : 0.F;
+    outputs[outputId].setVoltage(voltage);
+    outputs[negatedOutputId].setVoltage(10.F - voltage);
+  }
 
   static auto constexpr inputCount = 16;
 
--- src/modules/Gator.cpp
+++ src/modules/Gator.cpp
@@ -1,14 +1,49 @@
 #include "modules/Gator.h"
 
 #include "modules/controls/CommonConfig.h"
+#include "modules/controls/CommonInputs.h"
 
 #include <string>
 
-dhe::Gator::Gator() {
+namespace dhe {
+
+template <typename LogicOperator>
+auto reduce(std::vector<bool> const &values, bool identity, LogicOperator op) -> bool {
+  auto accumulated = identity;
+  for (auto const value : values) {
+    accumulated = op(accumulated, value);
+  }
+  return accumulated;
+}
+
+Gator::Gator() {
   config(ParameterCount, InputCount, OutputCount);
   for (auto i = 0; i < inputCount; i++) {
     configParam(NegateButtons + i, 0.F, 1.F, 0.F, "Negate input " + std::to_string(i + 1));
   }
 }
 
-void dhe::Gator::process(const rack::engine::Module::ProcessArgs &args) {}
+void Gator::process(const rack::engine::Module::ProcessArgs & /*ignored*/) {
+  auto connectedCount = 0;
+  auto highCount = 0;
+
+  for (int i = 0; i < inputCount; i++) {
+    if (inputs[i].isConnected()) {
+      connectedCount++;
+      if (inputIsHigh(this, Inputs + i) != buttonIsPressed(this, NegateButtons + i)) {
+        highCount++;
+      }
+    }
+  }
+
+  if (connectedCount == 0) {
+    setAllOutputsFalse();
+  } else {
+    setOutputs(AndOutput, NandOutput, highCount == connectedCount);
+    setOutputs(OrOutput, NorOutput, highCount > 0);
+    setOutputs(OddOutput, EvenOutput, (highCount & 1) > 0);
+    setOutputs(XorOutput, XnorOutput, highCount == 1);
+  }
+}
+
+} // namespace dhe
