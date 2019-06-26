01771c5510518385bada6f976c2f4d9e24e8dd80
Dale Emery <dale@dhemery.com>
2019-06-25 18:00:28 -0700

    New Flogic gui

--- gui/modules/flogic.rb
+++ gui/modules/flogic.rb
@@ -10,9 +10,8 @@ lc = hp2mm(3.25)
 rc = hp2mm(5.75)
 right = hp2mm(7.5)
 
-dy = 3
-
-top = 4.5
+top = 5
+dy = 4
 
 y = hp2mm(top + 0 * dy)
 port(x: left, y: y, label: 'A')
@@ -27,27 +26,20 @@ output_port(x: rc, y: y, label: '¬')
 output_port(x: right, y: y, label: 'A∨B')
 
 y = hp2mm(top + 2 * dy)
-output_port(x: lc, y: y, label: '¬')
-output_port(x: left, y: y, label: 'p(A⋂B)')
+output_port(x: left, y: y, label: '¬A')
+output_port(x: lc, y: y, label: '¬B')
 output_port(x: rc, y: y, label: '¬')
-output_port(x: right, y: y, label: 'p(A⋃B)')
+output_port(x: right, y: y, label: 'A⊻B')
 
-top += 10
-
-y = hp2mm(top + 0 * dy)
-port(x: left, y: y, label: 'C')
-button(x: lc, y: y, label: '¬')
-button(x: rc, y: y, label: '¬')
-port(x: right, y: y, label: 'D')
-
-y = hp2mm(top + 1 * dy)
-output_port(x: left, y: y, label: 'C∧D')
+y = hp2mm(top + 4 * dy)
+output_port(x: left, y: y, label: 'A⊃B')
 output_port(x: lc, y: y, label: '¬')
 output_port(x: rc, y: y, label: '¬')
-output_port(x: right, y: y, label: 'C∨D')
+output_port(x: right, y: y, label: 'A⊂B')
 
-y = hp2mm(top + 2 * dy)
+y = hp2mm(top + 3 * dy)
+output_port(x: left, y: y, label: 'A⋂B')
 output_port(x: lc, y: y, label: '¬')
-output_port(x: left, y: y, label: 'p(C⋂D)')
 output_port(x: rc, y: y, label: '¬')
-output_port(x: right, y: y, label: 'p(C⋃D)')
+output_port(x: right, y: y, label: 'A⋃B')
+
