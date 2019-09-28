64c2228170fd6a363ce0f187df30b1541139cfdb
Dale Emery <dale@dhemery.com>
2019-09-28 12:08:22 -0700

    Remove redundant inline declarations

--- include/modules/components/PhaseAccumulator.h
+++ include/modules/components/PhaseAccumulator.h
@@ -9,15 +9,15 @@ public:
     accumulated -= std::trunc(accumulated);
   }
 
-  inline auto phase() const -> float { return accumulated; }
+  auto phase() const -> float { return accumulated; }
 
-  inline auto angle(float offset = 0.F) -> float {
+  auto angle(float offset = 0.F) -> float {
     static float const twoPi{2.F * std::acos(-1.F)};
     return twoPi * (phase() + offset);
   }
 
-  inline auto sin(float offset = 0.F) -> float { return std::sin(angle(offset)); }
-  inline auto cos(float offset = 0.F) -> float { return std::cos(angle(offset)); }
+  auto sin(float offset = 0.F) -> float { return std::sin(angle(offset)); }
+  auto cos(float offset = 0.F) -> float { return std::cos(angle(offset)); }
 
 private:
   float accumulated{0.F};
