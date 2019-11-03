c102370c4f740aa3d641c65f8d6726999c7df447
Dale Emery <dale@dhemery.com>
2019-11-02 22:47:09 -0700

    More AdvancingMode tests

--- plugin/modules/curve-sequencer/AdvancingMode.h
+++ plugin/modules/curve-sequencer/AdvancingMode.h
@@ -1,7 +1,9 @@
 #pragma once
 
+#include "CurveSequencerControls.h"
 #include "Mode.h"
 #include "components/Latch.h"
+#include "controls/CommonInputs.h"
 
 #include <engine/Param.hpp>
 #include <engine/Port.hpp>
@@ -17,12 +19,26 @@ namespace curve_sequencer {
     AdvancingMode(std::vector<Input> &inputs, std::vector<Param> &params) : inputs{inputs}, params{params} {}
     auto isTerminal() const -> bool override { return false; }
 
-    auto execute(dhe::Latch const & /*runLatch*/, dhe::Latch const & /*gateLatch*/, int step,
+    auto execute(dhe::Latch const & /*runLatch*/, dhe::Latch const & /*gateLatch*/, int start,
                  float /*sampleTime*/) const -> Successor override {
-      return {ModeId::Generating, step};
+      auto const stop = selectionStart() + selectionLength();
+      for (int step = start; step < stop; step++) {
+        if (isEnabled(step)) {
+          return {ModeId::Generating, step};
+        }
+      }
+      return {ModeId::Idle, start};
     };
 
   private:
+    using Controls = CurveSequencerControls<N>;
+
+    auto selectionStart() const -> int { return static_cast<int>(valueOf(params[Controls::StartKnob])); }
+    auto selectionLength() const -> int { return static_cast<int>(valueOf(params[Controls::StepsKnob])); }
+
+    auto isEnabled(int step) const -> bool {
+      return isPressed(params[Controls::EnabledButtons + step]) || isHigh(inputs[Controls::EnabledInputs + step]);
+    }
     std::vector<Input> &inputs;
     std::vector<Param> &params;
   };
--- plugin/test/modules/curve-sequencer/AdvancingModeTest.cpp
+++ plugin/test/modules/curve-sequencer/AdvancingModeTest.cpp
@@ -7,7 +7,7 @@
 #include <engine/Port.hpp>
 #include <gtest/gtest.h>
 
-static auto constexpr stepCount{4};
+static auto constexpr stepCount{8};
 static auto constexpr unusedLatch = dhe::Latch{};
 
 using dhe::curve_sequencer::AdvancingMode;
@@ -24,46 +24,88 @@ struct AdvancingModeTest : public Test {
 
   void setEnabledButton(int step, bool state) { params[Controls::EnabledButtons + step].setValue(state ? 1.F : 0.F); }
   void setEnabledInput(int step, bool state) { inputs[Controls::EnabledInputs + step].setVoltage(state ? 10.F : 0.F); }
+  void setSelection(int start, int length) {
+    params[Controls::StartKnob].setValue(static_cast<float>(start));
+    params[Controls::StepsKnob].setValue(static_cast<float>(length));
+  }
 };
 
 TEST_F(AdvancingModeTest, isNotTerminal) { ASSERT_EQ(advancingMode.isTerminal(), false); }
 
-TEST_F(AdvancingModeTest, ifGivenStepEnabledByButton_nextStepIsGivenStep) {
+TEST_F(AdvancingModeTest, detectsStepEnabledByButton) {
+  auto const step = 4;
+
+  setSelection(0, stepCount);
+  setEnabledButton(step, true);
+
+  auto const next = advancingMode.execute(unusedLatch, unusedLatch, step, 0.F);
+
+  ASSERT_EQ(next.step, step);
+}
+
+TEST_F(AdvancingModeTest, detectsStepEnabledByInput) {
+  auto const step = 4;
+
+  setSelection(0, stepCount);
+  setEnabledInput(step, true);
+
+  auto const next = advancingMode.execute(unusedLatch, unusedLatch, step, 0.F);
+
+  ASSERT_EQ(next.step, step);
+}
+
+TEST_F(AdvancingModeTest, ifGivenStepIsEnabled_returnsGenerateGivenStep) {
   auto const step = 0;
 
+  setSelection(0, stepCount);
   setEnabledButton(step, true);
 
   auto const next = advancingMode.execute(unusedLatch, unusedLatch, step, 0.F);
 
-  ASSERT_EQ(next.step, step);
+  EXPECT_EQ(next.step, step);
+  EXPECT_EQ(next.modeId, ModeId::Generating);
 }
 
-TEST_F(AdvancingModeTest, ifGivenStepEnabledByButton_nextModeIsGenerating) {
-  auto const step = 1;
+TEST_F(AdvancingModeTest, ifFirstEnabledStepInSelectionIsAboveGivenStep_returnsGeneratingEnabledStep) {
+  auto const startStep = 2;
+  auto const firstEnabledStep = startStep + 2;
 
-  setEnabledButton(step, true);
+  setSelection(0, stepCount);
+  setEnabledButton(firstEnabledStep, true);
 
-  auto const next = advancingMode.execute(unusedLatch, unusedLatch, step, 0.F);
+  auto const next = advancingMode.execute(unusedLatch, unusedLatch, startStep, 0.F);
 
-  ASSERT_EQ(next.modeId, ModeId::Generating);
+  EXPECT_EQ(next.step, firstEnabledStep);
+  EXPECT_EQ(next.modeId, ModeId::Generating);
 }
 
-TEST_F(AdvancingModeTest, ifGivenStepEnabledByInput_nextStepIsGivenStep) {
-  auto const step = 2;
+TEST_F(AdvancingModeTest, ifFirstEnabledStepIsLastStepInSelection_returnsGeneratingEnabledStep) {
+  auto const selectionStart = 2;
+  auto const selectionLength = 3;
+  auto const lastStepInSelection = selectionStart + selectionLength - 1;
 
-  setEnabledInput(step, true);
+  setSelection(selectionStart, selectionLength);
+  setEnabledButton(lastStepInSelection, true);
 
-  auto const next = advancingMode.execute(unusedLatch, unusedLatch, step, 0.F);
+  auto const next = advancingMode.execute(unusedLatch, unusedLatch, selectionStart, 0.F);
 
-  ASSERT_EQ(next.step, step);
+  EXPECT_EQ(next.step, lastStepInSelection);
+  EXPECT_EQ(next.modeId, ModeId::Generating);
 }
 
-TEST_F(AdvancingModeTest, ifGivenStepEnabledByInput_nextModeIsGenerating) {
-  auto const step = 3;
+TEST_F(AdvancingModeTest, ifNoSelectedStepIsEnabled_returnsIdle) {
+  auto const selectionStart = 1;
+  auto const selectionLength = 4;
+  setSelection(selectionStart, selectionLength);
+  setEnabledButton(selectionStart + selectionLength, true); // Enabled but not in selection
 
-  setEnabledInput(step, true);
+  auto const next = advancingMode.execute(unusedLatch, unusedLatch, 0, 0.F);
 
-  auto const next = advancingMode.execute(unusedLatch, unusedLatch, step, 0.F);
-
-  ASSERT_EQ(next.modeId, ModeId::Generating);
+  EXPECT_EQ(next.modeId, ModeId::Idle);
 }
+
+// TODO: 6 012+]4[567 finds enabled step by wrapping
+// TODO: 5 0123]4[567 idle if no step enabled after wrapping
+// TODO: 1 0+23[456]7 idle if given step below selection
+// TODO: 6 0[123]45+7 idle if given step above selection
+// TODO: 4 01]23+5[67 idle if given step between selection
