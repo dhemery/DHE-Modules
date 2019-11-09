5f76d2624261267bf3c8e30596f2d537b4ba553c
Dale Emery <dale@dhemery.com>
2019-11-09 15:51:14 -0800

    Fix makefiles/travis.mk to include correct files

--- makefiles/travis.mk
+++ makefiles/travis.mk
@@ -1,8 +1,8 @@
 SELF_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
-include $(SELF_DIR)/dhe-files.mk
+include $(SELF_DIR)/file-definitions.mk
 
 DHE_CMAKE_OPTIONS += -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON
 
 DHE_CTEST_OPTIONS += --output-on-failure
 
-include $(SELF_DIR)/dhe-cmake.mk
+include $(SELF_DIR)/cmake-targets.mk
