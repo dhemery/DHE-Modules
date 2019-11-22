84ea19d96e0d80c5ada54475d1d442b518df6690
Dale Emery <dale@dhemery.com>
2019-11-22 15:42:54 -0800

    CS4 presets

--- makefiles/file-definitions.mk
+++ makefiles/file-definitions.mk
@@ -6,4 +6,4 @@ DHE_SOURCE_DIRS += plugin/modules/*
 DHE_SOURCE_FILES = $(foreach dir, $(DHE_SOURCE_DIRS), $(wildcard $(dir)/*.cpp))
 DHE_INCLUDE_FLAGS = -Iplugin/common/include
 
-DHE_DISTRIBUTABLES = LICENSE.txt svg
+DHE_DISTRIBUTABLES = LICENSE.txt presets svg
