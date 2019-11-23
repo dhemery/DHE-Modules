226c1a67bb5207b1fcb0b88c1fe4a793480a943e
Dale Emery <dale@dhemery.com>
2019-11-23 13:34:58 -0800

    Fix bad positioning of Ranger controls

--- plugin/modules/ranger/RangerPanel.cpp
+++ plugin/modules/ranger/RangerPanel.cpp
@@ -3,37 +3,35 @@
 namespace dhe {
 
 RangerPanel::RangerPanel(Ranger *module) : Panel{module, hp} {
-  auto widgetRightEdge = width();
-
-  auto constexpr column1 = mm2hp(1.5F);
-  auto column2 = widgetRightEdge - column1;
+  auto constexpr left = hp2mm(1.5F);
+  auto const right = width() - left;
 
   auto y = 14.5F;
   auto constexpr dy = 15.F;
 
   y += dy * 0.75F;
-  knob<MediumKnob>(column1, y, Ranger::LevelKnob);
-  output(column2, y, Ranger::RangerOutput);
+  knob<MediumKnob>(left, y, Ranger::LevelKnob);
+  output(right, y, Ranger::RangerOutput);
 
   y += dy;
-  input(column1, y, Ranger::LevelCvInput);
-  knob<TinyKnob>(column2, y, Ranger::LevelAvKnob);
+  input(left, y, Ranger::LevelCvInput);
+  knob<TinyKnob>(right, y, Ranger::LevelAvKnob);
 
   y += dy / 2.F;
   y += dy * 0.75F;
-  knob<MediumKnob>(column1, y, Ranger::CcwLimitKnob);
-  knob<MediumKnob>(column2, y, Ranger::CwLimitKnob);
+  knob<MediumKnob>(left, y, Ranger::CcwLimitKnob);
+  knob<MediumKnob>(right, y, Ranger::CwLimitKnob);
 
   y += dy;
-  knob<TinyKnob>(column1, y, Ranger::CcwLimitAvKnob);
-  knob<TinyKnob>(column2, y, Ranger::CwLimitAvKnob);
+  knob<TinyKnob>(left, y, Ranger::CcwLimitAvKnob);
+  knob<TinyKnob>(right, y, Ranger::CwLimitAvKnob);
 
   y += dy;
-  input(column1, y, Ranger::CcwLimitCvInput);
-  input(column2, y, Ranger::CwLimitCvInput);
+  input(left, y, Ranger::CcwLimitCvInput);
+  input(right, y, Ranger::CwLimitCvInput);
 
   y += dy;
-  toggle<2>(column1, y, Ranger::CcwLimitRangeSwitch);
-  toggle<2>(column2, y, Ranger::CwLimitRangeSwitch);
+  toggle<2>(left, y, Ranger::CcwLimitRangeSwitch);
+  toggle<2>(right, y, Ranger::CwLimitRangeSwitch);
 }
 } // namespace dhe
