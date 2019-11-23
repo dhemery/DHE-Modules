fc75d62cca8f3139393e844c993c370649078c6f
Dale Emery <dale@dhemery.com>
2019-11-22 23:15:26 -0800

    Module and Panel typedefs in PluginInitializer

--- plugin/modules/PluginInitializer.cpp
+++ plugin/modules/PluginInitializer.cpp
@@ -27,26 +27,37 @@ rack::plugin::Plugin *pluginInstance;
 
 using dhe::curve_sequencer::CurveSequencerModule;
 using dhe::curve_sequencer::CurveSequencerPanel;
+using dhe::func::Func1Panel;
+using dhe::func::Func6Panel;
+using dhe::func::FuncModule;
+using dhe::stage::BoosterStageModule;
+using dhe::stage::BoosterStagePanel;
+using dhe::stage::HostageModule;
+using dhe::stage::HostagePanel;
+using dhe::stage::StageModule;
+using dhe::stage::StagePanel;
+using dhe::stage::UpstageModule;
+using dhe::stage::UpstagePanel;
 
 void init(rack::plugin::Plugin *p) {
   pluginInstance = p;
 
   p->addModel(rack::createModel<dhe::Blossom, dhe::BlossomPanel>("Blossom"));
-  p->addModel(rack::createModel<dhe::stage::BoosterStageModule, dhe::stage::BoosterStagePanel>("BoosterStage"));
+  p->addModel(rack::createModel<BoosterStageModule, BoosterStagePanel>("BoosterStage"));
   p->addModel(rack::createModel<CurveSequencerModule<4>, CurveSequencerPanel<4>>("CurveSequencer4"));
   p->addModel(rack::createModel<CurveSequencerModule<8>, CurveSequencerPanel<8>>("CurveSequencer8"));
   p->addModel(rack::createModel<CurveSequencerModule<16>, CurveSequencerPanel<16>>("CurveSequencer16"));
   p->addModel(rack::createModel<dhe::Cubic, dhe::CubicPanel>("Cubic"));
-  p->addModel(rack::createModel<dhe::func::FuncModule<1>, dhe::func::Func1Panel>("Func"));
-  p->addModel(rack::createModel<dhe::func::FuncModule<6>, dhe::func::Func6Panel>("Func6"));
+  p->addModel(rack::createModel<FuncModule<1>, Func1Panel>("Func"));
+  p->addModel(rack::createModel<FuncModule<6>, Func6Panel>("Func6"));
   p->addModel(rack::createModel<dhe::FuzzyLogicH, dhe::FuzzyLogicHPanel>("FuzzyLogicH"));
   p->addModel(rack::createModel<dhe::FuzzyLogicZ, dhe::FuzzyLogicZPanel>("FuzzyLogicZ"));
   p->addModel(rack::createModel<dhe::Gator, dhe::GatorPanel>("Gator"));
-  p->addModel(rack::createModel<dhe::stage::HostageModule, dhe::stage::HostagePanel>("Hostage"));
-  p->addModel(rack::createModel<dhe::stage::StageModule, dhe::stage::StagePanel>("Stage"));
+  p->addModel(rack::createModel<HostageModule, HostagePanel>("Hostage"));
+  p->addModel(rack::createModel<StageModule, StagePanel>("Stage"));
   p->addModel(rack::createModel<dhe::Ranger, dhe::RangerPanel>("Ranger"));
   p->addModel(rack::createModel<dhe::Swave, dhe::SwavePanel>("Swave"));
   p->addModel(rack::createModel<dhe::Tapers, dhe::TapersPanel>("Tapers"));
-  p->addModel(rack::createModel<dhe::stage::UpstageModule, dhe::stage::UpstagePanel>("Upstage"));
+  p->addModel(rack::createModel<UpstageModule, UpstagePanel>("Upstage"));
   p->addModel(rack::createModel<dhe::Xycloid, dhe::XycloidPanel>("Xycloid"));
 }
