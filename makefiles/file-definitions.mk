55e44bcdb831149148a1ca47063e71df32f8451a
Dale Emery <dale@dhemery.com>
2020-01-15 22:22:43 -0800

    Fix source path

--- makefiles/file-definitions.mk
+++ makefiles/file-definitions.mk
@@ -1,6 +1,6 @@
-DHE_SOURCE_DIRS += plugin
+DHE_SOURCE_DIRS += src
 
 DHE_SOURCE_FILES = $(foreach dir, $(DHE_SOURCE_DIRS), $(wildcard $(dir)/*.cpp))
-DHE_INCLUDE_FLAGS = -Iplugin
+DHE_INCLUDE_FLAGS = -Isrc
 
 DHE_DISTRIBUTABLES = LICENSE.txt presets svg
