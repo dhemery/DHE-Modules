826506425b5a181708877c697fb2c51219b9fd4c
Dale Emery <dale@dhemery.com>
2020-02-10 22:35:42 -0800

    Func module guis use picklist instead of stepper

--- gui/modules/func-6.rb
+++ gui/modules/func-6.rb
@@ -7,7 +7,7 @@ center = width / 2.0
 left = width / 7.0
 right = width - left
 left_center = (left + center) / 2.0
-right_center = width - left_center
+right_center = width - left_center + 0.5
 
 row_count = 6
 last_row = row_count - 1
@@ -24,7 +24,7 @@ port_offset = 1.25
   input_port(x: left, y: y + port_offset)
   toggle(x: left_center, y: y, labels: %w(ADD MULT), selection: 1)
   large_knob(x: center, y: y, label: ' ')
-  stepper(x: right_center, y: y, name: 'add', labels: %w(0–5 ±5 0–10 ±10), selection: 2)
-  stepper(x: right_center, y: y, name: 'mult', labels: %w(0–1 ±1 0–2 ±2), selection: 2, hidden: true)
+  picklist x: right_center, y: y, name: 'add', labels: %w(0–5 ±5 0–10 ±10), selection: 2, width: 7
+  picklist x: right_center, y: y, name: 'mult', labels: %w(0–1 ±1 0–2 ±2), selection: 2, width: 7, hidden: true
   output_port(x: right, y: y + port_offset)
 end
