124ef84510a7f7ef5386e7c0566a539dbacbb349
Dale Emery <dale@dhemery.com>
2020-07-31 18:51:01 -0700

    Test controls with doctest

---
+++ test/controls/DurationInputTest.cpp
@@ -0,0 +1,49 @@
+#include "controls/DurationInputs.h"
+#include "doctest/doctest.h"
+
+namespace dhe {
+
+namespace durationtest {
+  class Knob {
+    float rotation;
+
+  public:
+    Knob(float rotation) : rotation{rotation} {};
+    auto getValue() const -> float { return rotation; }
+  };
+
+
+} // namespace durationtest
+
+TEST_CASE("duration") {
+  Range range = Range{2.1874F, 11.09234F};
+
+  SUBCASE("value at minimum rotation is range lower bound") {
+    durationtest::Knob knob{0.F};
+
+    auto const level = dhe::duration(knob, range);
+
+    auto const expected = range.lowerBound();
+
+    CHECK_EQ(level, doctest::Approx(expected).epsilon(0.000001F));
+  }
+
+  SUBCASE("value at middle rotation is near one tenth of the way into the range") {
+    durationtest::Knob knob{0.5F};
+
+    auto const level = dhe::duration(knob, range);
+
+    auto const expected = (range.upperBound() - range.lowerBound()) * 0.1F + range.lowerBound();
+    CHECK_EQ(level, doctest::Approx(expected).epsilon(0.005F));
+  }
+
+  SUBCASE("value at maximum rotation is range upper bound") {
+    durationtest::Knob knob{1.F};
+
+    auto const level = dhe::duration(knob, range);
+
+    auto const expected = range.upperBound();
+    CHECK_EQ(level, doctest::Approx(expected).epsilon(0.000001F));
+  }
+}
+} // namespace dhe
---
+++ test/controls/LevelInputTest.cpp
@@ -0,0 +1,53 @@
+#include "controls/LevelInputs.h"
+
+#undef INFO
+#undef WARN
+
+#include "doctest/doctest.h"
+namespace dhe {
+
+namespace leveltest {
+  class Knob {
+    float rotation;
+
+  public:
+    Knob(float rotation) : rotation{rotation} {};
+    auto getValue() const -> float { return rotation; }
+  };
+} // namespace leveltest
+
+TEST_CASE("level") {
+  // tolerance = 6 decimal places
+  static auto constexpr tolerance{0.000001F};
+
+  Range range = Range{3.8423F, 9.221F};
+
+  SUBCASE("value at minimum rotation is range lower bound") {
+    leveltest::Knob knob{0.F};
+
+    auto const level = dhe::level(knob, range);
+
+    auto const expected = range.lowerBound();
+
+    CHECK_EQ(level, doctest::Approx(expected).epsilon(tolerance));
+  }
+
+  SUBCASE("value at middle rotation is range midpoint") {
+    leveltest::Knob knob{0.5F};
+
+    auto const level = dhe::level(knob, range);
+
+    auto const expected = range.size() * 0.5 + range.lowerBound();
+    CHECK_EQ(level, doctest::Approx(expected).epsilon(tolerance));
+  }
+
+  SUBCASE("value at maximum rotation is range upper bound") {
+    leveltest::Knob knob{1.F};
+
+    auto const level = dhe::level(knob, range);
+
+    auto const expected = range.upperBound();
+    CHECK_EQ(level, doctest::Approx(expected).epsilon(tolerance));
+  }
+}
+} // namespace dhe
