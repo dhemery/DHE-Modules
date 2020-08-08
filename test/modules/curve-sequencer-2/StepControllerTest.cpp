feb2aae614807b97d2983ad9eda189dfa7fb4279
Dale Emery <dale@dhemery.com>
2020-08-08 15:17:34 -0700

    Move some helper functions to fake::Controls

--- test/modules/curve-sequencer-2/StepControllerTest.cpp
+++ test/modules/curve-sequencer-2/StepControllerTest.cpp
@@ -2,7 +2,6 @@
 
 #include "components/Latch.h"
 #include "components/PhaseTimer.h"
-#include "components/Taper.h"
 #include "doctest/doctest.h"
 #include "fake/Controls.h"
 #include "modules/curve-sequencer-2/TriggerMode.h"
@@ -18,7 +17,6 @@ namespace curve_sequencer_2 {
     using dhe::curve_sequencer::StepEvent;
     using dhe::curve_sequencer_2::StepController;
     using dhe::curve_sequencer_2::TriggerMode;
-    using dhe::taper::VariableTaper;
 
     static auto constexpr risingGate = Latch{true, true};
     static auto constexpr fallingGate = Latch{false, true};
@@ -31,20 +29,6 @@ namespace curve_sequencer_2 {
         TriggerMode::GateRises,  TriggerMode::GateFalls, TriggerMode::GateChanges,
         TriggerMode::GateIsHigh, TriggerMode::GateIsLow,
     };
-
-    static inline void prepareToGenerate(fake::Controls &controls) {
-      controls.interruptOnTrigger = [](int s) -> bool { return false; };
-      controls.advanceOnEndOfCurve = [](int s) -> bool { return false; };
-      controls.showProgress = [](int s, float f) {};
-
-      controls.curvature = [](int s) -> float { return 0.2F; };
-      controls.duration = [](int s) -> float { return 0.2F; };
-      controls.input = []() -> float { return 0.2F; };
-      controls.startLevel = [](int s) -> float { return 0.F; };
-      controls.endLevel = [](int s) -> float { return 10.F; };
-      controls.setOutput = [](float f) {};
-      controls.taper = [](int s) -> VariableTaper const * { return dhe::taper::variableTapers[0]; };
-    }
 
     TEST_CASE("curve_sequencer_2::StepController") {
       fake::Controls controls{};
@@ -73,11 +57,11 @@ namespace curve_sequencer_2 {
         controls.getOutput = []() -> float { return 5.5F; };
 
         auto constexpr step = 1;
-        controls.showProgress = [](int step, int progress) {};
+        controls.showProgress = [](int s, int p) {};
         stepController.enter(step);
 
         auto deactivatedStep = -1;
-        controls.showInactive = [&](int step) { deactivatedStep = step; };
+        controls.showInactive = [&](int s) { deactivatedStep = s; };
 
         stepController.exit();
 
@@ -86,27 +70,27 @@ namespace curve_sequencer_2 {
 
       SUBCASE("execute") {
         SUBCASE("when interruptible") {
-          controls.interruptOnTrigger = [](int s) -> bool { return true; };
-          controls.advanceOnEndOfCurve = [](int s) -> bool { return false; };
+          controls.interruptOnTrigger = fake::stepControlReturning(true);
+          controls.advanceOnEndOfCurve = fake::stepControlReturning(false);
           auto constexpr step{7};
           controls.showProgress = [](int s, float p) {};
           stepController.enter(step);
 
           SUBCASE("completes without generating if triggered") {
             SUBCASE("by rising gate") {
-              controls.triggerMode = [](int s) -> TriggerMode { return TriggerMode::GateRises; };
+              controls.triggerMode = fake::stepControlReturning(TriggerMode::GateRises);
               auto result = stepController.execute(risingGate, 0.F);
               CHECK_EQ(result, StepEvent::Completed);
             }
 
             SUBCASE("by falling gate") {
-              controls.triggerMode = [](int s) -> TriggerMode { return TriggerMode::GateFalls; };
+              controls.triggerMode = fake::stepControlReturning(TriggerMode::GateFalls);
               auto result = stepController.execute(fallingGate, 0.F);
               CHECK_EQ(result, StepEvent::Completed);
             }
 
             SUBCASE("by changing gate") {
-              controls.triggerMode = [](int s) -> TriggerMode { return TriggerMode::GateChanges; };
+              controls.triggerMode = fake::stepControlReturning(TriggerMode::GateChanges);
               auto result = stepController.execute(fallingGate, 0.F);
               CHECK_EQ(result, StepEvent::Completed);
               result = stepController.execute(risingGate, 0.F);
@@ -114,7 +98,7 @@ namespace curve_sequencer_2 {
             }
 
             SUBCASE("by high gate") {
-              controls.triggerMode = [](int s) -> TriggerMode { return TriggerMode::GateIsHigh; };
+              controls.triggerMode = fake::stepControlReturning(TriggerMode::GateIsHigh);
               auto result = stepController.execute(highGate, 0.F);
               CHECK_EQ(result, StepEvent::Completed);
               result = stepController.execute(risingGate, 0.F);
@@ -122,7 +106,7 @@ namespace curve_sequencer_2 {
             }
 
             SUBCASE("by low gate") {
-              controls.triggerMode = [](int s) -> TriggerMode { return TriggerMode::GateIsLow; };
+              controls.triggerMode = fake::stepControlReturning(TriggerMode::GateIsLow);
               auto result = stepController.execute(lowGate, 0.F);
               CHECK_EQ(result, StepEvent::Completed);
               result = stepController.execute(fallingGate, 0.F);
@@ -131,10 +115,10 @@ namespace curve_sequencer_2 {
           }
 
           SUBCASE("generates if not triggered") {
-            prepareToGenerate(controls);
+            allowGenerate(controls);
 
             SUBCASE("waiting for rising gate") {
-              controls.triggerMode = [](int s) -> TriggerMode { return TriggerMode::GateRises; };
+              controls.triggerMode = fake::stepControlReturning(TriggerMode::GateRises);
               auto result = stepController.execute(highGate, 0.F);
               CHECK_EQ(result, StepEvent::Generated);
               result = stepController.execute(fallingGate, 0.F);
@@ -144,7 +128,7 @@ namespace curve_sequencer_2 {
             }
 
             SUBCASE("waiting for falling gate") {
-              controls.triggerMode = [](int s) -> TriggerMode { return TriggerMode::GateFalls; };
+              controls.triggerMode = fake::stepControlReturning(TriggerMode::GateFalls);
               auto result = stepController.execute(lowGate, 0.F);
               CHECK_EQ(result, StepEvent::Generated);
               result = stepController.execute(highGate, 0.F);
@@ -154,7 +138,7 @@ namespace curve_sequencer_2 {
             }
 
             SUBCASE("waiting for changing gate") {
-              controls.triggerMode = [](int s) -> TriggerMode { return TriggerMode::GateChanges; };
+              controls.triggerMode = fake::stepControlReturning(TriggerMode::GateChanges);
               auto result = stepController.execute(lowGate, 0.F);
               CHECK_EQ(result, StepEvent::Generated);
               result = stepController.execute(highGate, 0.F);
@@ -162,7 +146,7 @@ namespace curve_sequencer_2 {
             }
 
             SUBCASE("waiting for high gate") {
-              controls.triggerMode = [](int s) -> TriggerMode { return TriggerMode::GateIsHigh; };
+              controls.triggerMode = fake::stepControlReturning(TriggerMode::GateIsHigh);
               auto result = stepController.execute(lowGate, 0.F);
               CHECK_EQ(result, StepEvent::Generated);
               result = stepController.execute(fallingGate, 0.F);
@@ -170,7 +154,7 @@ namespace curve_sequencer_2 {
             }
 
             SUBCASE("waiting for low gate") {
-              controls.triggerMode = [](int s) -> TriggerMode { return TriggerMode::GateIsLow; };
+              controls.triggerMode = fake::stepControlReturning(TriggerMode::GateIsLow);
               auto result = stepController.execute(highGate, 0.F);
               CHECK_EQ(result, StepEvent::Generated);
               result = stepController.execute(risingGate, 0.F);
@@ -180,16 +164,15 @@ namespace curve_sequencer_2 {
         }
 
         SUBCASE("when uninterruptible") {
-          prepareToGenerate(controls);
-          controls.interruptOnTrigger = [](int s) -> bool { return false; };
+          allowGenerate(controls);
+          controls.interruptOnTrigger = fake::stepControlReturning(false);
 
           SUBCASE("generates regardless of trigger") {
             std::for_each(triggerModes.cbegin(), triggerModes.cend(),
                           [&controls, &stepController](TriggerMode const triggerMode) {
                             std::for_each(gateStates.cbegin(), gateStates.cend(),
                                           [&controls, &stepController, triggerMode](Latch const &gateState) {
-                                            controls.triggerMode
-                                                = [triggerMode](int s) -> TriggerMode { return triggerMode; };
+                                            controls.triggerMode = fake::stepControlReturning(triggerMode);
                                             auto result = stepController.execute(gateState, 0.F);
                                             CHECK_EQ(result, StepEvent::Generated);
                                           });
@@ -199,14 +182,14 @@ namespace curve_sequencer_2 {
 
         SUBCASE("when advances on EOC") {
           auto constexpr step{2};
-          prepareToGenerate(controls);
+          allowGenerate(controls);
           stepController.enter(step);
-          controls.advanceOnEndOfCurve = [](int s) -> bool { return true; };
+          controls.advanceOnEndOfCurve = fake::stepControlReturning(true);
 
           SUBCASE("if curve completes") {
             timer.reset();
             timer.advance(0.99F);
-            controls.duration = [=](int s) -> float { return 1.F; };
+            controls.duration = fake::stepControlReturning(1.F);
             auto constexpr sampleTime{0.1F}; // Enough to complete the step
             controls.showInactive = [](int s) {};
 
@@ -230,7 +213,21 @@ namespace curve_sequencer_2 {
             }
           }
 
-          SUBCASE("if curve does not complete") {}
+          SUBCASE("if curve does not complete") {
+            timer.reset();
+            controls.duration = fake::stepControlReturning(1.F);
+            auto constexpr sampleTime{0.1F}; // Not enough to complete the step
+
+            SUBCASE("generates") {
+              return; // TODO:
+              auto output{-992.3F};
+              controls.setOutput = [&](float v) { output = v; };
+              stepController.execute(lowGate, sampleTime);
+              CHECK_EQ(output, controls.endLevel(step));
+            }
+            SUBCASE("reports generated") {}
+            SUBCASE("leaves step active") {}
+          }
         }
 
         SUBCASE("when sustains on EOC") {}
--- test/modules/curve-sequencer-2/fake/Controls.h
+++ test/modules/curve-sequencer-2/fake/Controls.h
@@ -27,12 +27,13 @@ namespace curve_sequencer_2 {
       auto output() const -> float { return getOutput(); }
       void output(float voltage) const { setOutput(voltage); };
 
-      // Step controls
+      // Step advancement controls
       std::function<TriggerMode(int step)> triggerMode{
           [](int s) -> TriggerMode { throw "Controls.triggerMode(step)"; }};
       std::function<bool(int)> interruptOnTrigger{[](int s) -> bool { throw "Controls.interruptOnTrigger(step)"; }};
       std::function<bool(int)> advanceOnEndOfCurve{[](int s) -> bool { throw "Controls.advanceOnEndOfCurve(step)"; }};
 
+      // Step generation controls
       std::function<float(int step)> curvature{[](int s) -> float { throw "Controls.curvature(step)"; }};
       std::function<float(int step)> duration{[](int s) -> float { throw "Controls.duration(step)"; }};
       std::function<float(int step)> startLevel{[](int s) -> float { throw "Controls.startLevel(step)"; }};
@@ -40,12 +41,31 @@ namespace curve_sequencer_2 {
       std::function<VariableTaper const *(int step)> taper{
           [](int s) -> VariableTaper const * { throw "Controls.taper(step)"; }};
 
+      // Step progress controls
       std::function<bool(int)> isEnabled{[](int s) -> bool { throw "Controls.isEnabled(step)"; }};
       std::function<void(int step, float progress)> showProgress{
           [](int s, float f) { throw "Controls.showProgress(step,progress)"; }};
       std::function<void(int step)> showInactive{[](int s) { throw "Controls.showInactive(step)"; }};
     };
 
+    inline void allowGenerate(Controls &controls) {
+      controls.advanceOnEndOfCurve = [](int s __attribute__((unused))) -> bool { return false; };
+      controls.interruptOnTrigger = [](int s __attribute__((unused))) -> bool { return false; };
+      controls.setOutput = [](float f __attribute__((unused))) {};
+      controls.showProgress = [](int s, float f) {};
+
+      controls.curvature = [](int s __attribute__((unused))) -> float { return 0.F; };
+      controls.duration = [](int s __attribute__((unused))) -> float { return 0.F; };
+      controls.input = []() -> float { return 0.F; };
+      controls.startLevel = [](int s __attribute__((unused))) -> float { return 0.F; };
+      controls.endLevel = [](int s __attribute__((unused))) -> float { return 0.F; };
+      controls.taper
+          = [](int s __attribute__((unused))) -> VariableTaper const * { return dhe::taper::variableTapers[0]; };
+    }
+
+    template <typename V> auto stepControlReturning(V v) -> std::function<V(int)> {
+      return [v](int s __attribute__((unused))) -> V { return v; };
+    }
   } // namespace fake
 } // namespace curve_sequencer_2
 } // namespace test
