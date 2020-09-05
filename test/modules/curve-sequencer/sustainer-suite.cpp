700822d8ee3cd76a08fb98d1f6ac19a064a1bfea
Dale Emery <dale@dhemery.com>
2020-09-04 22:21:41 -0700

    Table-driven sustainer tests

--- test/modules/curve-sequencer/sustainer-suite.cpp
+++ test/modules/curve-sequencer/sustainer-suite.cpp
@@ -1,155 +1,96 @@
+#include "./fixtures/completion-modes.h"
 #include "./fixtures/sustainer-fixture.h"
+#include "./fixtures/trigger-modes.h"
 #include "helpers/latches.h"
 #include <dheunit/assertions.h>
 #include <dheunit/test.h>
 
 namespace test {
 namespace curve_sequencer {
-using dhe::unit::is_false;
-using dhe::unit::is_true;
+using dhe::unit::is_equal_to;
 using dhe::unit::Suite;
 using dhe::unit::Tester;
 
+struct SustainerTestCase {
+  SustainerTestCase(CompletionMode completion_mode, TriggerMode trigger_mode,
+                    Latch latch, bool is_done)
+      : completion_mode_{completion_mode},
+        trigger_mode_{trigger_mode}, latch_{latch}, is_done_{is_done} {}
+  CompletionMode completion_mode_{}; // NOLINT
+  TriggerMode trigger_mode_{};       // NOLINT
+  Latch latch_{};                    // NOLINT
+  bool is_done_{};                   // NOLINT
+
+  void operator()(Tester &t) const {
+    auto constexpr step = 3;
+    Controls controls{};
+    Sustainer sustainer{controls};
+    controls.trigger_mode_[step] = trigger_mode_;
+    controls.completion_mode_[step] = completion_mode_;
+
+    t.assert_that(sustainer.is_done(step, latch_), is_equal_to(is_done_));
+  }
+
+  auto name() const -> std::string {
+    auto os = std::ostringstream{};
+    os << std::boolalpha << completion_mode_ << " + " << trigger_mode_ << " + "
+       << latch_ << " → " << is_done_;
+    return os.str();
+  }
+};
+
 class SustainerSuite : public Suite {
 public:
-  SustainerSuite() : Suite{"dhe::curve_sequencer::Sustainer"} {}
+  SustainerSuite() : Suite{"dhe::curve_sequencer::Sustainer is_done(s)"} {}
   void register_tests(dhe::unit::TestRegistrar add) override {
-    add("CompletionMode::Sustain, mode=GateRises, is_done(s) iff gate rises",
-        test([](Tester &t, Controls &controls, Sustainer &interrupter) {
-          auto constexpr step = 0;
-          controls.completion_mode_[step] = CompletionMode::Sustain;
-          controls.trigger_mode_[step] = TriggerMode::GateRises;
-
-          t.assert_that("rising gate", interrupter.is_done(step, rising_latch),
-                        is_true);
-          t.assert_that("falling gate",
-                        interrupter.is_done(step, falling_latch), is_false);
-          t.assert_that("high gate", interrupter.is_done(step, high_latch),
-                        is_false);
-          t.assert_that("low gate", interrupter.is_done(step, low_latch),
-                        is_false);
-        }));
-
-    add("CompletionMode::Sustain, mode=GateFalls, is_done(s) iff gate falls",
-        test([](Tester &t, Controls &controls, Sustainer &sustainer) {
-          auto constexpr step = 1;
-          controls.completion_mode_[step] = CompletionMode::Sustain;
-          controls.trigger_mode_[step] = TriggerMode::GateFalls;
-
-          t.assert_that("rising gate", sustainer.is_done(step, rising_latch),
-                        is_false);
-          t.assert_that("falling gate", sustainer.is_done(step, falling_latch),
-                        is_true);
-          t.assert_that("high gate", sustainer.is_done(step, high_latch),
-                        is_false);
-          t.assert_that("low gate", sustainer.is_done(step, low_latch),
-                        is_false);
-        }));
-
-    add("CompletionMode::Sustain, mode=GateChanges, is_done(s) iff gate "
-        "changes",
-        test([](Tester &t, Controls &controls, Sustainer &sustainer) {
-          auto constexpr step = 2;
-          controls.completion_mode_[step] = CompletionMode::Sustain;
-          controls.trigger_mode_[step] = TriggerMode::GateChanges;
-
-          t.assert_that("rising gate", sustainer.is_done(step, rising_latch),
-                        is_true);
-          t.assert_that("falling gate", sustainer.is_done(step, falling_latch),
-                        is_true);
-          t.assert_that("high gate", sustainer.is_done(step, high_latch),
-                        is_false);
-          t.assert_that("low gate", sustainer.is_done(step, low_latch),
-                        is_false);
-        }));
-
-    add("CompletionMode::Sustain, mode=GateIsHigh, is_done(s) iff gate is high",
-        test([](Tester &t, Controls &controls, Sustainer &sustainer) {
-          auto constexpr step = 3;
-          controls.completion_mode_[step] = CompletionMode::Sustain;
-          controls.trigger_mode_[step] = TriggerMode::GateIsHigh;
-
-          t.assert_that("rising gate", sustainer.is_done(step, rising_latch),
-                        is_true);
-          t.assert_that("falling gate", sustainer.is_done(step, falling_latch),
-                        is_false);
-          t.assert_that("high gate", sustainer.is_done(step, high_latch),
-                        is_true);
-          t.assert_that("low gate", sustainer.is_done(step, low_latch),
-                        is_false);
-        }));
-
-    add("CompletionMode::Sustain, mode=GateIsLow, is_done(s) iff gate is low",
-        test([](Tester &t, Controls &controls, Sustainer &sustainer) {
-          auto constexpr step = 4;
-          controls.completion_mode_[step] = CompletionMode::Sustain;
-          controls.trigger_mode_[step] = TriggerMode::GateIsLow;
-
-          t.assert_that("rising gate", sustainer.is_done(step, rising_latch),
-                        is_false);
-          t.assert_that("falling gate", sustainer.is_done(step, falling_latch),
-                        is_true);
-          t.assert_that("high gate", sustainer.is_done(step, high_latch),
-                        is_false);
-          t.assert_that("low gate", sustainer.is_done(step, low_latch),
-                        is_true);
-        }));
-
-    add("CompletionMode::Advance, is_done(s) regardless of mode or gate",
-        test([](Tester &t, Controls &controls, Sustainer &sustainer) {
-          auto constexpr step = 5;
-          controls.completion_mode_[step] = CompletionMode::Advance;
-
-          controls.trigger_mode_[step] = TriggerMode::GateRises;
-          t.assert_that("mode=GateRises: rising gate",
-                        sustainer.is_done(step, rising_latch), is_true);
-          t.assert_that("mode=GateRises: falling gate",
-                        sustainer.is_done(step, falling_latch), is_true);
-          t.assert_that("mode=GateRises: high gate",
-                        sustainer.is_done(step, high_latch), is_true);
-          t.assert_that("mode=GateRises: low gate",
-                        sustainer.is_done(step, low_latch), is_true);
-
-          controls.trigger_mode_[step] = TriggerMode::GateFalls;
-          t.assert_that("mode=GateFalls: rising gate",
-                        sustainer.is_done(step, rising_latch), is_true);
-          t.assert_that("mode=GateFalls: falling gate",
-                        sustainer.is_done(step, falling_latch), is_true);
-          t.assert_that("mode=GateFalls: high gate",
-                        sustainer.is_done(step, high_latch), is_true);
-          t.assert_that("mode=GateFalls: low gate",
-                        sustainer.is_done(step, low_latch), is_true);
-
-          controls.trigger_mode_[step] = TriggerMode::GateChanges;
-          t.assert_that("mode=GateChanges: rising gate",
-                        sustainer.is_done(step, rising_latch), is_true);
-          t.assert_that("mode=GateChanges: falling gate",
-                        sustainer.is_done(step, falling_latch), is_true);
-          t.assert_that("mode=GateChanges: high gate",
-                        sustainer.is_done(step, high_latch), is_true);
-          t.assert_that("mode=GateChanges: low gate",
-                        sustainer.is_done(step, low_latch), is_true);
-
-          controls.trigger_mode_[step] = TriggerMode::GateIsHigh;
-          t.assert_that("mode=GateIsHigh: rising gate",
-                        sustainer.is_done(step, rising_latch), is_true);
-          t.assert_that("mode=GateIsHigh: falling gate",
-                        sustainer.is_done(step, falling_latch), is_true);
-          t.assert_that("mode=GateIsHigh: high gate",
-                        sustainer.is_done(step, high_latch), is_true);
-          t.assert_that("mode=GateIsHigh: low gate",
-                        sustainer.is_done(step, low_latch), is_true);
-
-          controls.trigger_mode_[step] = TriggerMode::GateIsLow;
-          t.assert_that("mode=GateIsLow: rising gate",
-                        sustainer.is_done(step, rising_latch), is_true);
-          t.assert_that("mode=GateIsLow: falling gate",
-                        sustainer.is_done(step, falling_latch), is_true);
-          t.assert_that("mode=GateIsLow: high gate",
-                        sustainer.is_done(step, high_latch), is_true);
-          t.assert_that("mode=GateIsLow: low gate",
-                        sustainer.is_done(step, low_latch), is_true);
-        }));
+    auto test_cases = std::vector<SustainerTestCase>{
+        {CompletionMode::Sustain, TriggerMode::GateRises, rising_latch, true},
+        {CompletionMode::Sustain, TriggerMode::GateRises, falling_latch, false},
+        {CompletionMode::Sustain, TriggerMode::GateRises, high_latch, false},
+        {CompletionMode::Sustain, TriggerMode::GateRises, low_latch, false},
+        {CompletionMode::Sustain, TriggerMode::GateFalls, rising_latch, false},
+        {CompletionMode::Sustain, TriggerMode::GateFalls, falling_latch, true},
+        {CompletionMode::Sustain, TriggerMode::GateFalls, high_latch, false},
+        {CompletionMode::Sustain, TriggerMode::GateFalls, low_latch, false},
+        {CompletionMode::Sustain, TriggerMode::GateChanges, rising_latch, true},
+        {CompletionMode::Sustain, TriggerMode::GateChanges, falling_latch,
+         true},
+        {CompletionMode::Sustain, TriggerMode::GateChanges, high_latch, false},
+        {CompletionMode::Sustain, TriggerMode::GateChanges, low_latch, false},
+        {CompletionMode::Sustain, TriggerMode::GateIsHigh, rising_latch, true},
+        {CompletionMode::Sustain, TriggerMode::GateIsHigh, falling_latch,
+         false},
+        {CompletionMode::Sustain, TriggerMode::GateIsHigh, high_latch, true},
+        {CompletionMode::Sustain, TriggerMode::GateIsHigh, low_latch, false},
+        {CompletionMode::Sustain, TriggerMode::GateIsLow, rising_latch, false},
+        {CompletionMode::Sustain, TriggerMode::GateIsLow, falling_latch, true},
+        {CompletionMode::Sustain, TriggerMode::GateIsLow, high_latch, false},
+        {CompletionMode::Sustain, TriggerMode::GateIsLow, low_latch, true},
+        {CompletionMode::Advance, TriggerMode::GateRises, rising_latch, true},
+        {CompletionMode::Advance, TriggerMode::GateRises, falling_latch, true},
+        {CompletionMode::Advance, TriggerMode::GateRises, high_latch, true},
+        {CompletionMode::Advance, TriggerMode::GateRises, low_latch, true},
+        {CompletionMode::Advance, TriggerMode::GateFalls, rising_latch, true},
+        {CompletionMode::Advance, TriggerMode::GateFalls, falling_latch, true},
+        {CompletionMode::Advance, TriggerMode::GateFalls, high_latch, true},
+        {CompletionMode::Advance, TriggerMode::GateFalls, low_latch, true},
+        {CompletionMode::Advance, TriggerMode::GateChanges, rising_latch, true},
+        {CompletionMode::Advance, TriggerMode::GateChanges, falling_latch,
+         true},
+        {CompletionMode::Advance, TriggerMode::GateChanges, high_latch, true},
+        {CompletionMode::Advance, TriggerMode::GateChanges, low_latch, true},
+        {CompletionMode::Advance, TriggerMode::GateIsHigh, rising_latch, true},
+        {CompletionMode::Advance, TriggerMode::GateIsHigh, falling_latch, true},
+        {CompletionMode::Advance, TriggerMode::GateIsHigh, high_latch, true},
+        {CompletionMode::Advance, TriggerMode::GateIsHigh, low_latch, true},
+        {CompletionMode::Advance, TriggerMode::GateIsLow, rising_latch, true},
+        {CompletionMode::Advance, TriggerMode::GateIsLow, falling_latch, true},
+        {CompletionMode::Advance, TriggerMode::GateIsLow, high_latch, true},
+        {CompletionMode::Advance, TriggerMode::GateIsLow, low_latch, true},
+    };
+    for (auto const &test_case : test_cases) {
+      add(test_case.name(), test_case);
+    }
   }
 };
 
