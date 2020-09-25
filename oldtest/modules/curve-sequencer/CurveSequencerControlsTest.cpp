1ee4391374864cc40e35dfa0823318442835354b
Dale Emery <dale@dhemery.com>
2020-09-25 11:45:09 -0700

    Convert all global cs controls tests to dhe::unit

--- oldtest/modules/curve-sequencer/CurveSequencerControlsTest.cpp
+++ oldtest/modules/curve-sequencer/CurveSequencerControlsTest.cpp
@@ -24,90 +24,6 @@ namespace curve_sequencer_controls {
 
     Controls controls{inputs, outputs, params, lights};
 
-    SUBCASE("isGated()") {
-      SUBCASE("true if gate button is pressed") {
-        params[Controls::GateButton].setValue(1.F);
-
-        CHECK(controls.isGated());
-      }
-
-      SUBCASE("true if gate input is high") {
-        inputs[Controls::GateInput].setVoltage(10.F);
-
-        CHECK(controls.isGated());
-      }
-
-      SUBCASE("true if gate button is not pressed and gate input is low") {
-        params[Controls::GateButton].setValue(0.F);
-        inputs[Controls::GateInput].setVoltage(0.F);
-
-        CHECK_FALSE(controls.isGated());
-      }
-    }
-
-    SUBCASE("isLooping()") {
-      SUBCASE("true if loop button is pressed") {
-        params[Controls::LoopButton].setValue(1.F);
-
-        CHECK(controls.isLooping());
-      }
-
-      SUBCASE("true if loop input is high") {
-        inputs[Controls::LoopInput].setVoltage(10.F);
-
-        CHECK(controls.isLooping());
-      }
-
-      SUBCASE("true if loop button is not pressed and loop input is low") {
-        params[Controls::LoopButton].setValue(0.F);
-        inputs[Controls::LoopInput].setVoltage(0.F);
-
-        CHECK_FALSE(controls.isLooping());
-      }r
-    }
-
-    SUBCASE("isReset()") {
-      SUBCASE("true if reset button is pressed") {
-        params[Controls::ResetButton].setValue(1.F);
-
-        CHECK(controls.isReset());
-      }
-
-      SUBCASE("true if reset input is high") {
-        inputs[Controls::ResetInput].setVoltage(10.F);
-
-        CHECK(controls.isReset());
-      }
-
-      SUBCASE("true if reset button is not pressed and reset input is low") {
-        params[Controls::ResetButton].setValue(0.F);
-        inputs[Controls::ResetInput].setVoltage(0.F);
-
-        CHECK_FALSE(controls.isReset());
-      }
-    }
-
-    SUBCASE("isRunning()") {
-      SUBCASE("true if run button is pressed") {
-        params[Controls::RunButton].setValue(1.F);
-
-        CHECK(controls.isRunning());
-      }
-
-      SUBCASE("true if run input is high") {
-        inputs[Controls::RunInput].setVoltage(10.F);
-
-        CHECK(controls.isRunning());
-      }
-
-      SUBCASE("true if run button is not pressed and run input is low") {
-        params[Controls::RunButton].setValue(0.F);
-        inputs[Controls::RunInput].setVoltage(0.F);
-
-        CHECK_FALSE(controls.isRunning());
-      }
-    }
-
     SUBCASE("isEnabled(step)") {
       SUBCASE("true if enabled button is pressed") {
         auto constexpr step = 3;
@@ -131,36 +47,6 @@ namespace curve_sequencer_controls {
 
         CHECK_FALSE(controls.isEnabled(step));
       }
-    }
-
-    SUBCASE("selectionStart() reports selection start knob value") {
-      params[Controls::SelectionStartKnob].setValue(3.F);
-
-      CHECK_EQ(controls.selectionStart(), 3);
-    }
-
-    SUBCASE("selectionLength() reports selection length knob value") {
-      params[Controls::SelectionLengthKnob].setValue(5.F);
-
-      CHECK_EQ(controls.selectionLength(), 5);
-    }
-
-    SUBCASE("input() reports voltage at input port") {
-      inputs[Controls::CurveSequencerInput].setVoltage(5.3F);
-
-      CHECK_EQ(controls.input(), 5.3F);
-    }
-
-    SUBCASE("output() reports voltage at output port") {
-      outputs[Controls::CurveSequencerOutput].setVoltage(7.1F);
-
-      CHECK_EQ(controls.output(), 7.1F);
-    }
-
-    SUBCASE("output(voltage) sets voltage at output port") {
-      controls.output(5.3F);
-
-      CHECK_EQ(outputs[Controls::CurveSequencerOutput].getVoltage(), 5.3F);
     }
 
     SUBCASE("curvature(step)  reports curvature for step curvature param") {
--- test/modules/curve-sequencer/controls-suite.cpp
+++ test/modules/curve-sequencer/controls-suite.cpp
@@ -5,6 +5,7 @@
 namespace test {
 namespace curve_sequencer {
 
+using dhe::unit::is_equal_to;
 using dhe::unit::is_false;
 using dhe::unit::is_true;
 using dhe::unit::Suite;
@@ -29,13 +30,115 @@ public:
           t.assert_that(controls.is_gated(), is_true);
         }));
 
-    add("is_gated() is false if gate input is low and gate button is not "
-        "pressed",
+    add("is_gated() is false :"
+        "if gate input is low and gate button is not pressed",
         test([](Tester &t, Module &module, Controls &controls) {
           module.params_[Param ::GateButton].setValue(0.F);
           module.inputs_[Input::GateInput].setVoltage(0.F);
 
           t.assert_that(controls.is_gated(), is_false);
+        }));
+
+    add("is_looping() is true if loop button is pressed",
+        test([](Tester &t, Module &module, Controls &controls) {
+          module.params_[Param::LoopButton].setValue(1.F);
+
+          t.assert_that(controls.is_looping(), is_true);
+        }));
+
+    add("is_looping() is true if loop input is high",
+        test([](Tester &t, Module &module, Controls &controls) {
+          module.inputs_[Input::LoopInput].setVoltage(10.F);
+
+          t.assert_that(controls.is_looping(), is_true);
+        }));
+
+    add("is_looping() is false: "
+        "if loop input is low and loop button is not pressed",
+        test([](Tester &t, Module &module, Controls &controls) {
+          module.params_[Param ::LoopButton].setValue(0.F);
+          module.inputs_[Input::LoopInput].setVoltage(0.F);
+
+          t.assert_that(controls.is_looping(), is_false);
+        }));
+
+    add("is_reset() is true if reset button is pressed",
+        test([](Tester &t, Module &module, Controls &controls) {
+          module.params_[Param::ResetButton].setValue(1.F);
+
+          t.assert_that(controls.is_reset(), is_true);
+        }));
+
+    add("is_reset() is true if reset input is high",
+        test([](Tester &t, Module &module, Controls &controls) {
+          module.inputs_[Input::ResetInput].setVoltage(10.F);
+
+          t.assert_that(controls.is_reset(), is_true);
+        }));
+
+    add("is_reset() is false: "
+        "if reset input is low and reset button is not pressed",
+        test([](Tester &t, Module &module, Controls &controls) {
+          module.params_[Param ::ResetButton].setValue(0.F);
+          module.inputs_[Input::ResetInput].setVoltage(0.F);
+
+          t.assert_that(controls.is_reset(), is_false);
+        }));
+
+    add("is_running() is true if run button is pressed",
+        test([](Tester &t, Module &module, Controls &controls) {
+          module.params_[Param::RunButton].setValue(1.F);
+
+          t.assert_that(controls.is_running(), is_true);
+        }));
+
+    add("is_running() is true if run input is high",
+        test([](Tester &t, Module &module, Controls &controls) {
+          module.inputs_[Input::RunInput].setVoltage(10.F);
+
+          t.assert_that(controls.is_running(), is_true);
+        }));
+
+    add("is_running() is false: "
+        "if run input is low and run button is not pressed",
+        test([](Tester &t, Module &module, Controls &controls) {
+          module.params_[Param ::RunButton].setValue(0.F);
+          module.inputs_[Input::RunInput].setVoltage(0.F);
+
+          t.assert_that(controls.is_running(), is_false);
+        }));
+
+    add("selection_start()",
+        test([](Tester &t, Module &module, Controls &controls) {
+          module.params_[Param::SelectionStartKnob].setValue(3.F);
+          t.assert_that(controls.selection_start(), is_equal_to(3));
+        }));
+
+    add("selection_length()",
+        test([](Tester &t, Module &module, Controls &controls) {
+          module.params_[Param::SelectionLengthKnob].setValue(6.F);
+          t.assert_that(controls.selection_length(), is_equal_to(6));
+        }));
+
+    add("input()", test([](Tester &t, Module &module, Controls &controls) {
+          auto constexpr input_voltage = 3.1234F;
+          module.inputs_[Input::CurveSequencerInput].setVoltage(input_voltage);
+          t.assert_that(controls.input(), is_equal_to(input_voltage));
+        }));
+
+    add("output()", test([](Tester &t, Module &module, Controls &controls) {
+          auto constexpr output_voltage = 9.13894F;
+          module.outputs_[Output::CurveSequencerOutput].setVoltage(
+              output_voltage);
+          t.assert_that(controls.output(), is_equal_to(output_voltage));
+        }));
+
+    add("output(v)", test([](Tester &t, Module &module, Controls &controls) {
+          auto constexpr output_voltage = 4.390984F;
+          controls.output(output_voltage);
+          t.assert_that(
+              module.outputs_[Output::CurveSequencerOutput].getVoltage(),
+              is_equal_to(output_voltage));
         }));
   }
 };
