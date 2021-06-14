072b5d6a3d4ccbd4d19601d46ffd8896216eebf5
Dale Emery <dale@dhemery.com>
2021-06-13 23:29:05 -0700

    Another structure for CS tests

--- test/modules/curve-sequencer/curve-sequencer-suite.cpp
+++ test/modules/curve-sequencer/curve-sequencer-suite.cpp
@@ -33,8 +33,6 @@ struct StateSuite {
 // CurveSequencer starts idle, so this is a no-op.
 static inline void become_idle(Context &, CurveSequencer &) {} // NOLINT
 
-static void activate_step(Context &context, CurveSequencer &cs); // NOLINT
-
 // Note: Context throws if a command is called without being explicitly allowed
 auto idle_tests = StateSuite{
     .name_ = "Idle",
@@ -47,13 +45,11 @@ auto idle_tests = StateSuite{
                     [](Context &context) {
                       context.controls_.is_running_ = true;
                       context.controls_.is_reset_ = true;
-                      context.controls_.input_ = 3.9434F;
-                      context.step_controller_.step_ = -2451;
                     },
                 .check_ =
                     [](Context &context) {
-                      context.controls_.assert_output(3.9434F);
-                      context.step_controller_.assert_step(-2451); // NOLINT
+                      context.controls_.assert_output(original_input);
+                      context.step_controller_.assert_step(original_step);
                     },
             },
             {
@@ -62,13 +58,11 @@ auto idle_tests = StateSuite{
                     [](Context &context) {
                       context.controls_.is_running_ = true;
                       context.controls_.is_reset_ = false;
-                      context.controls_.output_ = -221.F;
-                      context.step_controller_.step_ = 7124; // NOLINT
                     },
                 .check_ =
                     [](Context &context) {
-                      context.controls_.assert_output(-221.F);
-                      context.step_controller_.assert_step(7124); // NOLINT
+                      context.controls_.assert_output(original_output);
+                      context.step_controller_.assert_step(original_step);
                     },
             },
             {
@@ -80,8 +74,8 @@ auto idle_tests = StateSuite{
                     },
                 .check_ =
                     [](Context &context) {
-                      context.controls_.assert_output(-221.F);
-                      context.step_controller_.assert_step(7124); // NOLINT
+                      context.controls_.assert_output(original_output);
+                      context.step_controller_.assert_step(original_step);
                     },
             },
             {
@@ -108,11 +102,10 @@ auto idle_tests = StateSuite{
                       context.controls_.is_running_ = true;
                       context.controls_.is_gated_ = true; // Causes rise
                       context.step_selector_.first_ = no_step;
-                      context.step_controller_.step_ = -2471;
                     },
                 .check_ =
                     [](Context &context) {
-                      context.step_controller_.assert_step(-2471); // unchanged
+                      context.step_controller_.assert_step(original_step);
                     },
             },
             {
@@ -151,106 +144,133 @@ auto idle_tests = StateSuite{
     ,
 };
 
-auto active_tests = StateSuite{
-    .name_ = "Active",
-    .enter_state_ = activate_step,
+struct StateTest2 {
+  std::string name_;               // NOLINT
+  std::vector<Context2> contexts_; // NOLINT
+  std::vector<Check2> checks_;     // NOLINT
+  void run(Tester &, Module2 &, CurveSequencer &) const;
+};
+
+struct StateSuite2 {
+  std::string name_;               // NOLINT
+  std::vector<Context2> contexts_; // NOLINT
+  std::vector<StateTest2> tests_;  // NOLINT
+  void run(Tester &t) const;
+};
+
+auto active_tests = StateSuite2{
+    .name_ = "NewActive",
+    .contexts_ =
+        {
+            activate_step(2),
+        },
     .tests_ =
         {
             {
-                .name_ = "paused: gate low does nothing",
-                .setup_ =
-                    [](Context &context) {
-                      context.controls_.is_running_ = false;
-                      context.controls_.is_gated_ = false;
+                .name_ = "does nothing if not running and gate is low",
+                .contexts_ =
+                    {
+                        {is_running(false)},
+                        {is_gated(false)},
                     },
-            },
-            {
-                .name_ = "paused: gate rise does nothing",
-                .setup_ =
-                    [](Context &context) {
-                      context.controls_.is_running_ = false;
-                      context.controls_.is_gated_ = true;
-                    },
-            },
-            {
-                .name_ = "paused: reset low does nothing",
-                .setup_ =
-                    [](Context &context) {
-                      context.controls_.is_running_ = false;
-                      context.controls_.is_reset_ = false;
-                    },
-            },
-            {
-                .name_ = "paused: "
-                         "reset rise exits active step and does nothing else",
-                .setup_ =
-                    [](Context &context) {
-                      context.controls_.is_running_ = false;
-                      context.controls_.is_reset_ = true;
-                      context.step_controller_.want_exit();
-                    },
-            },
-            {
-                .name_ = "running: "
-                         "executes active step with gate state",
-                .setup_ =
-                    [](Context &context) {
-                      context.controls_.is_running_ = true;
-                      context.controls_.is_gated_ = true;
-                      // Resets gate edge on first call to execute()
-                      context.step_controller_.want_execute(
-                          StepEvent::Generated, high_latch, 0.1F);
-                      // TODO: Other gate states: rising, falling, low
-                      // TODO: Fix magic constant 0.1F
-                    },
-            },
-            {
-                .name_ = "running: "
-                         "enters successor state if step completes",
-                .setup_ =
-                    [](Context &context) {
-                      auto constexpr current_step = 2;
-                      auto constexpr successor_step = 4;
-                      context.controls_.is_running_ = true;
-                      context.controls_.is_gated_ = true;
-
-                      context.step_controller_.want_execute(
-                          StepEvent::Completed, high_latch, 0.1F);
-                      context.step_selector_.want_successor(successor_step,
-                                                            current_step);
-                      context.step_controller_.want_enter(successor_step);
-                    },
-            },
-            {
-                .name_ = "running: step completes: no successor: "
-                         "exit current step and does nothing else",
-                .setup_ =
-                    [](Context &context) {
-                      auto constexpr current_step = 2;
-                      context.controls_.is_running_ = true;
-                      context.controls_.is_gated_ = true;
-
-                      context.step_controller_.want_execute(
-                          StepEvent::Completed, high_latch, 0.1F);
-                      context.step_selector_.want_successor(-1, current_step);
-                    },
-            },
-            {
-                .name_ = "running: reset rises: "
-                         "copies input to output, exits current step, "
-                         "and does nothing else",
-                .setup_ =
-                    [](Context &context) {
-                      context.controls_.is_running_ = true;
-                      context.controls_.is_reset_ = true;
-                      context.controls_.input_ = 5.8340F;
-
-                      context.step_controller_.want_exit();
-                      context.controls_.want_output(5.8340F);
+                .checks_ =
+                    {
+                        {assert_output(original_output)},
+                        {assert_step(original_step)},
                     },
             },
         },
 };
+
+/**
+        {
+            .name_ = "paused: gate rise does nothing",
+            .setup_ =
+                [](Context &context) {
+                  context.controls_.is_running_ = false;
+                  context.controls_.is_gated_ = true;
+                },
+        },
+        {
+            .name_ = "paused: reset low does nothing",
+            .setup_ =
+                [](Context &context) {
+                  context.controls_.is_running_ = false;
+                  context.controls_.is_reset_ = false;
+                },
+        },
+        {
+            .name_ = "paused: "
+                     "reset rise exits active step and does nothing else",
+            .setup_ =
+                [](Context &context) {
+                  context.controls_.is_running_ = false;
+                  context.controls_.is_reset_ = true;
+                  context.step_controller_.want_exit();
+                },
+        },
+        {
+            .name_ = "running: "
+                     "executes active step with gate state",
+            .setup_ =
+                [](Context &context) {
+                  context.controls_.is_running_ = true;
+                  context.controls_.is_gated_ = true;
+                  // Resets gate edge on first call to execute()
+                  context.step_controller_.want_execute(StepEvent::Generated,
+                                                        high_latch, 0.1F);
+                  // TODO: Other gate states: rising, falling, low
+                  // TODO: Fix magic constant 0.1F
+                },
+        },
+        {
+            .name_ = "running: "
+                     "enters successor state if step completes",
+            .setup_ =
+                [](Context &context) {
+                  auto constexpr current_step = 2;
+                  auto constexpr successor_step = 4;
+                  context.controls_.is_running_ = true;
+                  context.controls_.is_gated_ = true;
+
+                  context.step_controller_.want_execute(StepEvent::Completed,
+                                                        high_latch, 0.1F);
+                  context.step_selector_.want_successor(successor_step,
+                                                        current_step);
+                  context.step_controller_.want_enter(successor_step);
+                },
+        },
+        {
+            .name_ = "running: step completes: no successor: "
+                     "exit current step and does nothing else",
+            .setup_ =
+                [](Context &context) {
+                  auto constexpr current_step = 2;
+                  context.controls_.is_running_ = true;
+                  context.controls_.is_gated_ = true;
+
+                  context.step_controller_.want_execute(StepEvent::Completed,
+                                                        high_latch, 0.1F);
+                  context.step_selector_.want_successor(-1, current_step);
+                },
+        },
+        {
+            .name_ = "running: reset rises: "
+                     "copies input to output, exits current step, "
+                     "and does nothing else",
+            .setup_ =
+                [](Context &context) {
+                  context.controls_.is_running_ = true;
+                  context.controls_.is_reset_ = true;
+                  context.controls_.input_ = 5.8340F;
+
+                  context.step_controller_.want_exit();
+                  context.controls_.want_output(5.8340F);
+                },
+        },
+},
+};
+*/
 
 struct CurveSequencerSuite : public Suite {
   CurveSequencerSuite() : Suite{"dhe::curve_suquencer::CurveSequencer"} {}
@@ -265,7 +285,6 @@ void StateTest::run(Tester &t, Context &context,
   t.run(name_, [this, &context, &curve_sequencer](Tester &t) {
     setup_(context);
     curve_sequencer.execute(0.1F);
-    context.check_required_calls();
   });
 }
 
@@ -286,17 +305,45 @@ void StateSuite::run(Tester &t) const {
   });
 }
 
-// TODO: Randomize the active step, and arrange for the test to know which step
-//  is active
-void activate_step(Context &context, CurveSequencer &cs) {
-  context.controls_.is_running_ = true;
-  // Gate rise will trigger CS to enter first step and execute it.
-  context.controls_.is_gated_ = true;
-  auto constexpr first_step = 2;
-  context.step_selector_.first_ = first_step;
-  context.step_controller_.step_event_ = StepEvent::Generated;
+void StateTest2::run(Tester &t, Module2 &m, CurveSequencer &cs) const {
+  t.run(name_, [this, &m, &cs](Tester &t) {
+    // Execute cs in each of the test's contexts.
+    for (auto const &context : contexts_) {
+      for (auto const &condition : context) {
+        condition(m);
+      }
+      cs.execute(0.F);
+    }
+    // Apply each check to the final state.
+    for (auto const &check : checks_) {
+      check(t, m);
+    }
+  });
+}
 
-  cs.execute(0.F);
+void StateSuite2::run(Tester &t) const {
+  t.run(name_, [this](Tester &t) {
+    for (auto const &test : tests_) {
+      auto controls = Controls{t};
+      auto step_selector = StepSelector{t};
+      auto step_controller = StepController{t};
+
+      auto module = Module2{controls, step_selector, step_controller};
+
+      auto curve_sequencer =
+          CurveSequencer{controls, step_selector, step_controller};
+
+      // To set up the suite's initial state, execute cs in each of the suite's
+      // contexts.
+      for (auto const &context : contexts_) {
+        for (auto const &condition : context) {
+          condition(module);
+        }
+        curve_sequencer.execute(0.F);
+      }
+      test.run(t, module, curve_sequencer);
+    }
+  });
 }
 
 static auto _ = CurveSequencerSuite{};
--- test/modules/curve-sequencer/fixtures/curve-sequencer-fixture.h
+++ test/modules/curve-sequencer/fixtures/curve-sequencer-fixture.h
@@ -16,15 +16,32 @@ using dhe::Latch;
 using dhe::curve_sequencer::StepEvent;
 using dhe::unit::Tester;
 static auto constexpr step_count = 8;
-static auto constexpr initial_step = 999;
 static auto constexpr no_step = -1;
 
+// Initial values for input variables. These values are invalid. If a test wants
+// the engine to use a variable, it will overwrite the value with a valid one.
+// So if the engine uses these original values for anything, then either the
+// test or the engine did something wrong.
+static auto constexpr original_first = 2222;
+static auto constexpr original_successor = 3333;
+static auto constexpr original_input = 1.111F;
+static auto constexpr original_step_event = static_cast<StepEvent>(5555);
+
+// Initial values for output variables that the engine will overwrite under the
+// right conditions. Except for original_gate, neither the tests nor the engine
+// will write these values. If a test wants to verify that the engine left a
+// variable unchanged, it can assert that the variable has its original value.
+static auto constexpr original_step = 1111;
+static auto constexpr exited_step = 4444;
+static auto constexpr original_output = 2.222F;
+static auto constexpr original_gate = falling_latch;
+
 struct Controls {
-  bool is_gated_{};       // NOLINT
-  bool is_reset_{};       // NOLINT
-  bool is_running_{};     // NOLINT
-  float input_ = -199.F;  // NOLINT
-  float output_ = -299.F; // NOLINT
+  bool is_gated_{};                // NOLINT
+  bool is_reset_{};                // NOLINT
+  bool is_running_{};              // NOLINT
+  float input_ = original_input;   // NOLINT
+  float output_ = original_output; // NOLINT
 
   Controls(Tester &t) : t_{t} {}
 
@@ -46,12 +63,12 @@ private:
 };
 
 struct StepController {
-  int first_ = initial_step;                    // NOLINT
-  StepEvent step_event_ = StepEvent::Completed; // NOLINT
+  int first_ = original_first;                 // NOLINT
+  StepEvent step_event_ = original_step_event; // NOLINT
 
-  int step_{};          // NOLINT
-  Latch gate_{};        // NOLINT
-  float sample_time_{}; // NOLINT
+  int step_ = original_step;   // NOLINT
+  Latch gate_ = original_gate; // NOLINT
+  float sample_time_{};        // NOLINT
 
   StepController(Tester &t) : t_{t} {}
 
@@ -67,7 +84,7 @@ struct StepController {
     return step_event_;
   }
 
-  void exit() { step_ = no_step; }
+  void exit() { step_ = exited_step; }
 
   void assert_step(int want) {
     if (step_ != want) {
@@ -86,12 +103,12 @@ private:
 };
 
 struct StepSelector {
-  int first_ = initial_step;               // NOLINT
+  int first_ = original_step;              // NOLINT
   std::array<int, step_count> successors_; // NOLINT
 
   StepSelector(Tester &t) : t_{t} {
     for (auto &successor : successors_) {
-      successor = initial_step;
+      successor = original_successor;
     }
   }
 
@@ -126,5 +143,59 @@ using CurveSequencer =
 using SetState = std::function<void(Context &, CurveSequencer &)>;
 using SetConditions = std::function<void(Context &)>;
 using Check = std::function<void(Context &)>;
+
+struct Module2 {
+  Controls controls_;
+  StepSelector step_selector_;
+  StepController step_controller_;
+};
+
+using Condition2 = std::function<void(Module2 &)>;
+using Context2 = std::vector<Condition2>;
+using Check2 = std::function<void(Tester &, Module2 &)>;
+
+static inline auto is_running(bool b) -> Condition2 {
+  return [b](Module2 &module) { module.controls_.is_running_ = b; };
+}
+
+static inline auto is_gated(bool b) -> Condition2 {
+  return [b](Module2 &module) { module.controls_.is_gated_ = b; };
+}
+
+static inline auto first_step(int s) -> Condition2 {
+  return [s](Module2 &module) { module.step_selector_.first_ = s; };
+}
+
+static inline auto step_event(StepEvent e) -> Condition2 {
+  return [e](Module2 &module) { module.step_controller_.step_event_ = e; };
+}
+
+static inline auto assert_output(float want) -> Check2 {
+  return [want](Tester &t, Module2 &module) {
+    float got = module.controls_.output_;
+    if (got != want) {
+      t.errorf("Got output {}, want {}", got, want);
+    }
+  };
+}
+
+static inline auto assert_step(int want) -> Check2 {
+  return [want](Tester &t, Module2 &module) {
+    int got = module.step_controller_.step_;
+    if (got != want) {
+      t.errorf("Got step {}, want {}", got, want);
+    }
+  };
+}
+
+static inline auto activate_step(int step) -> Context2 {
+  return {
+      is_running(true),
+      is_gated(true),
+      first_step(step),
+      step_event(StepEvent::Generated),
+  };
+}
+
 } // namespace curve_sequencer
 } // namespace test
