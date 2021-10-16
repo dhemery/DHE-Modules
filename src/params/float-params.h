539064e49056276a787f514ba3acbc3e73403d60
Dale Emery <dale@dhemery.com>
2021-10-16 11:30:11 -0700

    Fix Attenuverter scale

--- src/params/float-params.h
+++ src/params/float-params.h
@@ -8,16 +8,16 @@ namespace dhe {
 struct Attenuator {
   static constexpr auto range = Range{0.F, 1.F};
   static inline void config(rack::engine::Module *module, int id,
-                            std::string const &name, float rotation = 1.F) {
-    module->configParam(id, 0.F, 1.F, rotation, name, "%", 0.F, 100.F, 0.F);
+                            std::string const &name) {
+    module->configParam(id, 0.F, 1.F, 1.F, name, "%", 0.F, 100.F, 0.F);
   }
 };
 
 struct Attenuverter {
   static constexpr auto range = Range{-1.F, 1.F};
   static inline void config(rack::engine::Module *module, int id,
-                            std::string const &name, float rotation = 1.F) {
-    module->configParam(id, 0.F, 1.F, rotation, name, "%", -100.F, 100.F, 0.F);
+                            std::string const &name) {
+    module->configParam(id, 0.F, 1.F, 1.F, name, "%", 0.F, 200.F, -100.F);
   }
 };
 } // namespace dhe
