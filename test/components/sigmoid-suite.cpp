c988805b456531225baa44684f5ee961ea4468f7
Dale Emery <dale@dhemery.com>
2021-11-20 16:00:00 -0800

    The rest of the shape tests

--- test/components/sigmoid-suite.cpp
+++ test/components/sigmoid-suite.cpp
@@ -16,8 +16,6 @@ using dhe::unit::Suite;
 using dhe::unit::Tester;
 using test::is_near;
 
-static auto constexpr tolerance = 0.000001F;
-
 struct ShapeTest {
   float input;
   float output;
@@ -26,8 +24,10 @@ struct ShapeTest {
     auto const name = std::string{"{"} + std::to_string(input) + "," +
                       std::to_string(output) + "}";
     t.run(name, [this, curvature](Tester &t) {
-      auto const got = S::apply(input, curvature);
-      assert_that(t, got, is_near(output, tolerance));
+      auto const applied = S::apply(input, curvature);
+      assert_that(t, applied, is_near(output, 0.000001F));
+      auto const inverted = S::invert(applied, curvature);
+      assert_that(t, inverted, is_near(input, 0.001F));
     });
   }
 };
@@ -47,32 +47,28 @@ template <typename S> struct TaperSuite {
   }
 };
 
-// Characterize each taper by using as input:
-// 0.00 The lower bound of the input range
-// 0.05 Just above the lower bound
-// 0.45 Just below the middle
-// 0.50 The middle
-// 0.55 Just above the middle
-// 0.95 Just below the upper bound
-// 1.00 The upper bound
-
 // Curvature 0 creates a linear taper for both J and S shapes
 static auto zero_curvature_tests = std::vector<ShapeTest>{
     {0.F, 0.F},     {0.05F, 0.05F}, {0.45F, 0.45F}, {0.5F, 0.5F},
     {0.55F, 0.55F}, {0.95F, 0.95F}, {1.F, 1.F},
 };
 
+// Characterize each J taper with these inputs:
+// 0.00 The lower bound of the input range
+// 0.05 Just above the lower bound
+// 0.50 The middle
+// 0.95 Just below the upper bound
+// 1.00 The upper bound
 static auto j_shape_tests = std::vector<TaperSuite<JShape>>{
     {
         .name = "JShape::min-curvature",
         .curvature = -0.9999F,
         .tests =
             {
+                // Slope is very steep near the low bound, very low elsewhere
                 {0.F, 0.F},
                 {0.05F, 0.999051F},
-                {0.45F, 0.999939F},
                 {0.5F, 0.999950F},
-                {0.55F, 0.999959F},
                 {0.95F, 0.999997F},
                 {1.F, 1.F},
             },
@@ -82,12 +78,11 @@ static auto j_shape_tests = std::vector<TaperSuite<JShape>>{
         .curvature = -0.8F,
         .tests =
             {
+                // Slope increases slowly at first, then quickly
                 {0.F, 0.F},
-                {0.05F, 0.05F},
-                {0.45F, 0.45F},
-                {0.5F, 0.5F},
-                {0.55F, 0.55F},
-                {0.95F, 0.95F},
+                {0.05F, 0.321429F},
+                {0.5F, 0.9F},
+                {0.95F, 0.994186F},
                 {1.F, 1.F},
             },
     },
@@ -101,12 +96,11 @@ static auto j_shape_tests = std::vector<TaperSuite<JShape>>{
         .curvature = 0.8F,
         .tests =
             {
+                // Slope increases slowly at first, then quickly
                 {0.F, 0.F},
-                {0.05F, 0.05F},
-                {0.45F, 0.45F},
-                {0.5F, 0.5F},
-                {0.55F, 0.55F},
-                {0.95F, 0.95F},
+                {0.05F, 0.00581395F},
+                {0.5F, 0.1F},
+                {0.95F, 0.678571F},
                 {1.F, 1.F},
             },
     },
@@ -115,17 +109,26 @@ static auto j_shape_tests = std::vector<TaperSuite<JShape>>{
         .curvature = 0.9999F,
         .tests =
             {
+                // Slope is very steep near the high bound, very low elsewhere
                 {0.F, 0.F},
-                {0.05F, 0.05F},
-                {0.45F, 0.45F},
-                {0.5F, 0.5F},
-                {0.55F, 0.55F},
-                {0.95F, 0.95F},
+                {0.05F, 2.63136e-06F},
+                {0.5F, 5.00083e-05F},
+                {0.95F, 0.000949243F},
                 {1.F, 1.F},
             },
     },
 };
 
+// Characterize each S taper with these inputs:
+// 0.00 The lower bound of the input range
+// 0.05 Just above the lower bound
+// 0.25 The middle of the lower half
+// 0.45 Just below the middle
+// 0.50 The middle
+// 0.55 Just above the middle
+// 0.75 The middle of the upper half
+// 0.95 Just below the upper bound
+// 1.00 The upper bound
 static auto s_shape_tests = std::vector<TaperSuite<SShape>>{
     {
         .name = "SShape minimum curvature (-0.9999)",
@@ -135,9 +138,11 @@ static auto s_shape_tests = std::vector<TaperSuite<SShape>>{
                 // Slope is very high at the very ends, very low elsewhere
                 {0.F, 0.F},
                 {0.05F, 0.499775F},
+                {0.25F, 0.499975F},
                 {0.45F, 0.499997F},
                 {0.5F, 0.5F},
                 {0.55F, 0.500003F},
+                {0.75F, 0.500025F},
                 {0.95F, 0.500225F},
                 {1.F, 1.F},
             },
@@ -147,12 +152,14 @@ static auto s_shape_tests = std::vector<TaperSuite<SShape>>{
         .curvature = -0.8F,
         .tests =
             {
-                // Slope is llow in the center, high at ends
+                // Slope is low in the center, high at ends
                 {0.F, 0.F},
                 {0.05F, 0.25F},
+                {0.25F, 0.45F},
                 {0.45F, 0.493902F},
                 {0.5F, 0.5F},
                 {0.55F, 0.506098F},
+                {0.75F, 0.55F},
                 {0.95F, 0.75F},
                 {1.F, 1.F},
             },
@@ -170,9 +177,11 @@ static auto s_shape_tests = std::vector<TaperSuite<SShape>>{
                 // Slope is high in the center, low at ends
                 {0.F, 0.F},
                 {0.05F, 6.09756e-3F},
+                {0.25F, 0.05F},
                 {0.45F, 0.25F},
                 {0.5F, 0.5F},
                 {0.55F, 0.75F},
+                {0.75F, 0.95F},
                 {0.95F, 0.993902F},
                 {1.F, 1.F},
             },
@@ -185,9 +194,11 @@ static auto s_shape_tests = std::vector<TaperSuite<SShape>>{
                 // Slope is very high in very center, very low elsewhere
                 {0.F, 0.F},
                 {0.05F, 2.77162e-6F},
+                {0.25F, 2.50041e-5F},
                 {0.45F, 2.24888e-4F},
                 {0.5F, 0.5F},
                 {0.55F, 0.999775F},
+                {0.75F, 0.999975F},
                 {0.95F, 0.999997F},
                 {1.F, 1.F},
             },
