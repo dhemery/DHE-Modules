1701fe1af91e042b5d25a55565d6e3788c89c6b4
Dale Emery <dale@dhemery.com>
2021-11-23 21:46:54 -0800

    Simplify signals/basic.h

--- src/signals/basic.h
+++ src/signals/basic.h
@@ -18,31 +18,19 @@ template <typename I> auto is_high(I const &input) -> bool {
   return voltage_at(input) > 1.F;
 }
 
-static inline auto rotation_of(float rotation, float cv,
-                               float cv_multiplier = 1.F) -> float {
-  static auto constexpr rotation_per_volt = 0.1F;
-  auto const modulation = cv * rotation_per_volt * cv_multiplier;
-  return rotation + modulation;
-}
-
 template <typename P> auto rotation_of(P const &knob) -> float {
   return value_of(knob);
 }
 
 template <typename P, typename I>
 auto rotation_of(P const &knob, I const &cv_input) -> float {
-  auto const rotation = value_of(knob);
-  auto const cv = voltage_at(cv_input);
-  return rotation_of(rotation, cv);
+  return value_of(knob) + voltage_at(cv_input) * 0.1F;
 }
 
 template <typename P, typename I>
 auto rotation_of(P const &knob, I const &cv_input, P const &av_knob) -> float {
-  auto const rotation = value_of(knob);
-  auto const cv = voltage_at(cv_input);
-  auto const av_rotation = value_of(av_knob);
-  auto const multiplier = Attenuverter::scale(av_rotation);
-  return rotation_of(rotation, cv, multiplier);
+  return value_of(knob) +
+         voltage_at(cv_input) * 0.1F * Attenuverter::scale(value_of(av_knob));
 }
 
 } // namespace dhe
