49e0a2c92765fe6f07b380f4ca8623646f9446b0
Dale Emery <dale@dhemery.com>
2021-11-24 22:00:38 -0800

    Fix Ranger default rotations

--- src/modules/ranger/module.h
+++ src/modules/ranger/module.h
@@ -19,12 +19,12 @@ struct Module : public rack::engine::Module {
   Module() {
     config(ParamId::Count, InputId::Count, OutputId::Count);
 
-    Knob::config<Percentage>(this, ParamId::Level, "Level");
+    Knob::config<Percentage>(this, ParamId::Level, "Level", 50.F);
     configInput(InputId::LevelCv, "Level CV");
     Knob::config<Attenuverter>(this, ParamId::LevelAv, "Level CV gain");
 
     auto *ccw_limit_knob =
-        Knob::config<Voltage>(this, ParamId::CcwLimit, "CCW limit", 0.F);
+        Knob::config<Voltage>(this, ParamId::CcwLimit, "CCW limit", -5.F);
     auto select_ccw_limit_range = [ccw_limit_knob](VoltageRangeId id) {
       ccw_limit_knob->mapper().select_range(id);
     };
@@ -35,7 +35,7 @@ struct Module : public rack::engine::Module {
     Knob::config<Attenuverter>(this, ParamId::CcwLimitAv, "CCW limit CV gain");
 
     auto *cw_limit_knob =
-        Knob::config<Voltage>(this, ParamId::CwLimit, "CW limit", 1.F);
+        Knob::config<Voltage>(this, ParamId::CwLimit, "CW limit", 5.F);
     auto select_cw_limit_range = [cw_limit_knob](VoltageRangeId id) {
       cw_limit_knob->mapper().select_range(id);
     };
